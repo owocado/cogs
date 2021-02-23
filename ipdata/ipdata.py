a305f35c503bd02c150fd6ccc4eb0014d62bfeb8
Slave In Utero <siu3334@users.noreply.github.com>
2021-02-23 19:15:08 +0530

    ipdata fix

--- ipdata/ipdata.py
+++ ipdata/ipdata.py
@@ -1,4 +1,3 @@
-from ipdata import ipdata
 from typing import Any, Dict, Literal
 
 import aiohttp
@@ -51,40 +50,42 @@ class IPData(commands.Cog):
         """Returns various info about provided IP."""
         api = await ctx.bot.get_shared_api_tokens("ipdata")
         key = api.get("api_key")
-        ipdata = ipdata.IPData(str(key))
-        response = ipdata.lookup(ip)
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f"https://api.ipdata.co/{ip}?api-key={key}") as response:
+                data = await response.json()
 
-        try:
-            em = discord.Embed(color=await ctx.embed_colour())
-            em.set_author(
-                name=f"Info for IP: {response.get('ip')}",
-                icon_url=str(response.get("flag")),
-            )
-            em.add_field(name="ASN (Carrier)", value=str(response.get("asn").get("name")))
-            em.add_field(name="ASN Type", value=str(response.get("asn").get("type")))
-            em.add_field(name="ASN Domain", value=str(response.get("asn").get("domain")))
-            em.add_field(name="ASN Route", value=str(response.get("asn").get("route")))
-            em.add_field(name="City", value=str(response.get("city")))
-            em.add_field(name="Region", value=str(response.get("region")))
-            em.add_field(name="Country", value=str(response.get("country_name")))
-            em.add_field(name="Continent", value=str(response.get("continent_name")))
-            em.add_field(name="Calling Code", value=str(response.get("calling_code")))
-            threat_info = "\u200b"
-            if response.get("threat").get("is_anonymous"):
-                threat_info += "Is anonymous? \N{WHITE HEAVY CHECK MARK}"
-            if response.get("threat").get("is_bogon"):
-                threat_info += "Is Bogon? \N{WHITE HEAVY CHECK MARK}"
-            if response.get("threat").get("is_known_abuser"):
-                threat_info += "Is Known abuser? \N{WHITE HEAVY CHECK MARK}"
-            if response.get("threat").get("is_known_attacker"):
-                threat_info += "Is attacker? \N{WHITE HEAVY CHECK MARK}"
-            if response.get("threat").get("is_proxy"):
-                threat_info += "Is proxy? \N{WHITE HEAVY CHECK MARK}"
-            if response.get("threat").get("is_threat"):
-                threat_info += "Is threat? \N{WHITE HEAVY CHECK MARK}"
-            if response.get("threat").get("is_tor"):
-                threat_info += "Is TOR? \N{WHITE HEAVY CHECK MARK}"
-            em.description = threat_info
-            return await ctx.send(embed=em)
-        except ValueError as e:
-            await ctx.send(str(e))
+                if response.status == 200:
+                    em = discord.Embed(color=await ctx.embed_colour())
+                    em.set_author(
+                        name=f"Info for IP: {data.get('ip')}",
+                        icon_url=str(data.get("flag")),
+                    )
+                    em.add_field(name="ASN (Carrier)", value=str(data.get("asn").get("name")))
+                    em.add_field(name="ASN Type", value=str(data.get("asn").get("type")))
+                    em.add_field(name="ASN Domain", value=str(data.get("asn").get("domain")))
+                    em.add_field(name="ASN Route", value=str(data.get("asn").get("route")))
+                    em.add_field(name="City", value=str(data.get("city")))
+                    em.add_field(name="Region", value=str(data.get("region")))
+                    em.add_field(name="Country", value=str(data.get("country_name")))
+                    em.add_field(name="Continent", value=str(data.get("continent_name")))
+                    em.add_field(name="Calling Code", value=str(data.get("calling_code")))
+                    threat_info = "\u200b"
+                    if data.get("threat").get("is_anonymous"):
+                        threat_info += "Is anonymous? \N{WHITE HEAVY CHECK MARK}"
+                    if data.get("threat").get("is_bogon"):
+                        threat_info += "Is Bogon? \N{WHITE HEAVY CHECK MARK}"
+                    if data.get("threat").get("is_known_abuser"):
+                        threat_info += "Is Known abuser? \N{WHITE HEAVY CHECK MARK}"
+                    if data.get("threat").get("is_known_attacker"):
+                        threat_info += "Is attacker? \N{WHITE HEAVY CHECK MARK}"
+                    if data.get("threat").get("is_proxy"):
+                        threat_info += "Is proxy? \N{WHITE HEAVY CHECK MARK}"
+                    if data.get("threat").get("is_threat"):
+                        threat_info += "Is threat? \N{WHITE HEAVY CHECK MARK}"
+                    if data.get("threat").get("is_tor"):
+                        threat_info += "Is TOR? \N{WHITE HEAVY CHECK MARK}"
+                    em.description = threat_info
+                    return await ctx.send(embed=em)
+                else:
+                    error = data.get("message")
+                    await ctx.send(str(error))
