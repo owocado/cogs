2727cc841e8f691a3f7421c243db494405427388
Willow Jenkins <24418520+jenkins420@users.noreply.github.com>
2021-03-25 14:29:40 +0530

    fix and improve indexerror when no lyrics returned

--- vocadb/vocadb.py
+++ vocadb/vocadb.py
@@ -19,13 +19,13 @@ RequestType = Literal["discord_deleted_user", "owner", "user", "user_strict"]
 class VocaDB(commands.Cog):
     """Search for a song on Vocaloid Database (VocaDB) through a query"""
 
-    __author__ = ["siu3334", "<@306810730055729152>"]
-    __version__ = "0.0.1"
+    __author__ = "<@306810730055729152>"
+    __version__ = "0.0.2"
 
     def format_help_for_context(self, ctx: commands.Context) -> str:
         """Thanks Sinbad!"""
         pre_processed = super().format_help_for_context(ctx)
-        return f"{pre_processed}\n\nCog Version: {self.__version__}"
+        return f"{pre_processed}\n\nAuthor: {self.__author__}\nCog Version: {self.__version__}"
 
     def __init__(self, bot: Red):
         self.bot = bot
@@ -54,37 +54,44 @@ class VocaDB(commands.Cog):
         }
         async with aiohttp.ClientSession() as session:
             async with session.get(BASE_API_URL, params=params) as response:
-                result = await response.json(loads=json.loads)
+                if response.status == 200:
+                    result = await response.json(loads=json.loads)
+                else:
+                    return await ctx.send(f"API returned response code: {response.status}")
 
-        try:
-            data = result["items"][0]
-        except IndexError:
+        # Thanks to Fixator's suggestion
+        if data := result.get("items"):
+            data = data[0]
+        else:
             return await ctx.send("No results.")
-        lyrics = data["lyrics"][0]["value"]
-        mins = data["lengthSeconds"] // 60
-        secs = data["lengthSeconds"] % 60
-        pub_date = data["publishDate"]
+        if lyrics := data.get("lyrics"):
+            lyrics = lyrics[0].get("value")
+        else:
+            lyrics = "Lyrics unavailable."
+        mins = data.get("lengthSeconds", 0) // 60
+        secs = data.get("lengthSeconds", 0) % 60
+        pub_date = data.get("publishDate")
         pub_date_strp = datetime.strptime(pub_date, "%Y-%m-%dT%H:%M:%SZ")
         pub_date_strf = pub_date_strp.strftime("%A, %d %B, %Y")
 
         embeds = []
         for page in pagify(lyrics, ["\n"]):
             em = discord.Embed(
-                title=f"{data['defaultName']} by {data['artistString']}",
+                title=f"{data.get('defaultName')}",
                 colour=await ctx.embed_colour(),
             )
-            em.url = f"https://vocadb.net/S/{data['id']}"
+            em.url = f"https://vocadb.net/S/{data.get('id')}"
             em.description = page
-            em.set_thumbnail(url=str(data["thumbUrl"]))
-            # em.add_field(name="Artist", value=str(data["artistString"]))
+            em.set_thumbnail(url=str(data.get("thumbUrl")))
             em.add_field(name="Duration", value=f"{mins} minutes, {secs} seconds")
-            em.add_field(name="Rating Score", value=str(data['ratingScore']))
+            em.add_field(name="Rating Score", value=str(data.get('ratingScore')))
             em.add_field(
                 name="Favourited",
-                value=f"{str(data['favoritedTimes'])} times"
+                value=f"{str(data.get('favoritedTimes'))} times"
                 if data.get("favoritedTimes")
                 else "0 times"
             )
+            em.add_field(name="Artist", value=str(data.get("artistString")))
             em.set_footer(text=f"Published date: {pub_date_strf}")
             embeds.append(em)
 
