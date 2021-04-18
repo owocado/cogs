ae2427555d8133be34430f974655040f8cf5a41f
Willow Jenkins <24418520+jenkins420@users.noreply.github.com>
2021-04-18 22:44:08 +0530

    ty Fixator 😘 https://i.imgur.com/Y6quKKT.png

--- country/country.py
+++ country/country.py
@@ -1,84 +1,110 @@
 import aiohttp
-from typing import Any, Dict, Literal
 
 # Required by Red
 import discord
-from redbot.core import checks, commands
+from redbot.core import commands
 from redbot.core.bot import Red
 from redbot.core.utils.chat_formatting import humanize_number, pagify
 from redbot.core.utils.menus import menu, DEFAULT_CONTROLS
-
-RequestType = Literal["discord_deleted_user", "owner", "user", "user_strict"]
 
 
 class Country(commands.Cog):
     """Various commands to show the stats about users' profile badges."""
 
-    __author__ = ["siu3334", "<@306810730055729152>"]
-    __version__ = "0.0.3"
+    __author__ = "<@306810730055729152>"
+    __version__ = "0.0.4"
 
     def format_help_for_context(self, ctx: commands.Context) -> str:
         """Thanks Sinbad!"""
         pre_processed = super().format_help_for_context(ctx)
-        return f"{pre_processed}\n\nCog Version: {self.__version__}"
+        return f"{pre_processed}\n\n Author: {self.__author__}\n Cog Version: {self.__version__}"
 
     def __init__(self, bot: Red):
         self.bot = bot
-
-    # credits to jack1142
-    async def red_get_data_for_user(self, *, user_id: int) -> Dict[str, Any]:
-        # this cog does not story any data
-        return {}
-
-    # credits to jack1142
-    async def red_delete_data_for_user(
-        self, *, requester: RequestType, user_id: int
-    ) -> None:
-        # this cog does not story any data
-        pass
 
     @commands.command()
     @commands.guild_only()
     @commands.bot_has_permissions(embed_links=True)
     async def country(self, ctx: commands.Context, name: str):
         """Shows various info about a country."""
+        async with ctx.typing():
+            try:
+                async with aiohttp.ClientSession() as session:
+                    async with session.get(
+                        f"https://restcountries.eu/rest/v2/name/{name}"
+                    ) as response:
+                        if response.status == 200:
+                            result = await response.json()
+                        else:
+                            return await ctx.send(f"https://http.cat/{response.status}")
+            except asyncio.TimeoutError:
+                return await ctx.send("Operation timed out.")
 
-        async with aiohttp.ClientSession() as session:
-            async with session.get(f"https://restcountries.eu/rest/v2/name/{name}") as response:
-                result = await response.json()
-
-        embed_list = []
-        for data in result:
-            embed = discord.Embed(colour=await ctx.embed_color())
-            embed.title = data["name"]
-            embed.set_footer(text="Powered by REST Countries API!")
-            embed.add_field(name="Population", value=humanize_number(data["population"]))
-            embed.add_field(name="Calling Code", value=f"\u200b{data['callingCodes'][0]}")
-            embed.add_field(name="Capital", value=f"\u200b{data['capital']}")
-            embed.add_field(name="Currency", value=data["currencies"][0]["name"])
-            embed.add_field(
-                name="Region / Subregion",
-                value=f"{data.get('region', 'None')} / {data.get('subregion', 'None')}",
-            )
-            embed.add_field(name="Top Level Domain", value=data["topLevelDomain"][0])
-            if data["gini"] is not None:
-                embed.add_field(name="GINI Index", value=data.get("gini", "None"))
-            embed.add_field(name="Demonym", value=data.get("demonym", "None"))
-            embed.add_field(name="Native Name", value=data.get("nativeName", "None"))
-            if data["area"] is not None:
-                embed.add_field(name="Approx. Area", value=f"{humanize_number(data['area'])} km²", inline=False)
-            tzones = ", ".join(x for x in data["timezones"])
-            embed.add_field(name="Timezones", value=f"{tzones}", inline=False)
-            if data["borders"]:
-                borders = ", ".join(x for x in data["borders"])
-                embed.add_field(name="Shares borders with", value=f"{borders}", inline=False)
-            if data["altSpellings"]:
-                altnames = ", ".join(x for x in data["altSpellings"])
-                embed.add_field(name="Other Names", value=f"{altnames}", inline=False)
-            if data["regionalBlocs"]:
-                embed.add_field(name="Part of Trade Bloc", value=data["regionalBlocs"][0]["name"], inline=False)
-            embed.set_thumbnail(url=f"https://www.countryflags.io/{data['alpha2Code']}/flat/64.png")
-            embed_list.append(embed)
+            embed_list = []
+            for data in result:
+                embed = discord.Embed(colour=await ctx.embed_color())
+                embed.title = data.get("name", "None")
+                embed.set_footer(text="Powered by REST Countries API!")
+                embed.add_field(
+                    name="Population", value=humanize_number(data.get("population", 0))
+                )
+                embed.add_field(
+                    name="Calling Code", value="+" + data.get("callingCodes")[0]
+                )
+                if data.get("capital") != "":
+                    embed.add_field(name="Capital", value=data.get("capital"))
+                embed.add_field(
+                    name="Currency",
+                    value=data.get("currencies")[0].get("name")
+                    + " ("
+                    + data.get("currencies")[0].get("code")
+                    + ")",
+                )
+                embed.add_field(
+                    name="Region / Subregion",
+                    value=data.get("region", "None")
+                    + " / "
+                    + data.get("subregion", "None"),
+                )
+                embed.add_field(
+                    name="Top Level Domain", value=data.get("topLevelDomain")[0]
+                )
+                if data.get("gini") is not None:
+                    embed.add_field(name="GINI Index", value=data.get("gini"))
+                if data.get("demonym") != "":
+                    embed.add_field(name="Demonym", value=data.get("demonym", "None"))
+                embed.add_field(
+                    name="Native Name", value=data.get("nativeName", "None")
+                )
+                if data.get("area") is not None:
+                    embed.add_field(
+                        name="Approx. Area",
+                        value=f"{humanize_number(data.get('area'))} km²",
+                        inline=False,
+                    )
+                tzones = ", ".join(data.get("timezones"))
+                embed.add_field(name="Timezones", value=tzones, inline=False)
+                if data.get("borders") != []:
+                    borders = ", ".join(data.get("borders"))
+                    embed.add_field(
+                        name="Shares borders with", value=borders, inline=False
+                    )
+                if data.get("altSpellings"):
+                    alt_names = ", ".join(data.get("altSpellings"))
+                    embed.add_field(
+                        name="Other Names", value=alt_names, inline=False
+                    )
+                if data.get("regionalBlocs"):
+                    blocs = ", ".join(
+                        [x.get("name") for x in data.get("regionalBlocs")]
+                    )
+                    embed.add_field(
+                        name="Part of Trade Bloc", value=blocs, inline=False
+                    )
+                embed.set_thumbnail(
+                    url=f"https://www.countryflags.io/{data.get('alpha2Code')}/flat/64.png"
+                )
+                embed_list.append(embed)
 
         if not embed_list:
             return await ctx.send("No results.")
