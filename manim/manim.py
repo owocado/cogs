72b63935e2d607e9417d44245e5e35639bf65b37
Willow Jenkins <24418520+jenkins420@users.noreply.github.com>
2021-04-19 14:17:09 +0530

    make manim non-blocking

--- manim/manim.py
+++ manim/manim.py
@@ -1,11 +1,13 @@
 import asyncio
 import docker
+import functools
 import os
 import tempfile
 import re
 import io
 
 from pathlib import Path
+from typing import Optional
 
 import discord
 from redbot.core import commands
@@ -21,6 +23,14 @@ dockerclient = docker.from_env()
 class Manim(commands.Cog):
     """A cog for interacting with Manim python animation engine."""
 
+    __author__ = "<@306810730055729152>"
+    __version__ = "0.0.2"
+
+    def format_help_for_context(self, ctx: commands.Context) -> str:
+        """Thanks Sinbad!"""
+        pre_processed = super().format_help_for_context(ctx)
+        return f"{pre_processed}\n\nAuthor: {self.__author__}\nCog Version: {self.__version__}"
+
     def __init__(self, bot):
         self.bot = bot
 
@@ -28,7 +38,7 @@ class Manim(commands.Cog):
     @commands.is_owner()
     @commands.command(aliases=["manim"])
     @commands.bot_has_permissions(attach_files=True)
-    async def manimate(self, ctx: commands.Context, *, arg: str):
+    async def manimate(self, ctx: commands.Context, *, snippet: str):
         """Render short and simple Manim scripts.
         Code **must** be properly formatted and indented. Note that you can't animate through DM's.
 
@@ -43,115 +53,120 @@ class Manim(commands.Cog):
             self.play(ReplacementTransform(Square(), Circle()))
         ```
         """
+        async with ctx.typing():
+            task = functools.partial(self.construct_reply, arg=snippet)
+            task = self.bot.loop.run_in_executor(None, task)
+            try:
+                reply_args = await asyncio.wait_for(task, timeout=120)
+            except asyncio.TimeoutError:
+                return await ctx.send("Operation timed out.")
 
-        def construct_reply(arg):
-            if arg.startswith("```"):  # empty header
-                arg = "\n" + arg
-            header, *body = arg.split("\n")
+        await ctx.reply(**reply_args)
 
-            cli_flags = header.split()
-            allowed_flags = [
-                "-i",
-                "--save_as_gif",
-                "-s",
-                "--save_last_frame",
-                "-t",
-                "--transparent",
+    def construct_reply(self, arg: str) -> Optional[discord.File]:
+        if arg.startswith("```"):  # empty header
+            arg = "\n" + arg
+        header, *body = arg.split("\n")
+
+        cli_flags = header.split()
+        allowed_flags = [
+            "-i",
+            "--save_as_gif",
+            "-s",
+            "--save_last_frame",
+            "-t",
+            "--transparent",
+        ]
+        if not all([flag in allowed_flags for flag in cli_flags]):
+            reply_args = {
+                "content": "You cannot pass CLI flags other than "
+                "`-i` (`--save_as_gif`), `-s` (`--save_last_frame`), "
+                "`-t` (`--transparent`)."
+            }
+            return reply_args
+        else:
+            cli_flags = " ".join(cli_flags)
+
+        body = "\n".join(body).strip()
+
+        if body.count("```") != 2:
+            reply_args = {
+                "content": "Your message is not properly formatted. "
+                "Your code has to be written in a code block, like so:\n"
+                "\\`\\`\\`py\nyour code here\n\\`\\`\\`"
+            }
+            return reply_args
+
+        script = re.search(
+            pattern=r"```(?:py)?(?:thon)?(.*)```",
+            string=body,
+            flags=re.DOTALL,
+        ).group(1)
+        script = script.strip()
+
+        # for convenience: allow construct-only:
+        if script.startswith("def construct(self):"):
+            script = ["class Manimation(Scene):"] + [
+                "    " + line for line in script.split("\n")
             ]
-            if not all([flag in allowed_flags for flag in cli_flags]):
-                reply_args = {
-                    "content": "You cannot pass CLI flags other than "
-                    "`-i` (`--save_as_gif`), `-s` (`--save_last_frame`), "
-                    "`-t` (`--transparent`)."
-                }
-                return reply_args
-            else:
-                cli_flags = " ".join(cli_flags)
+        else:
+            script = script.split("\n")
 
-            body = "\n".join(body).strip()
+        script = ["from manim import *"] + script
 
-            if body.count("```") != 2:
-                reply_args = {
-                    "content": "Your message is not properly formatted. "
-                    "Your code has to be written in a code block, like so:\n"
-                    "\\`\\`\\`py\nyour code here\n\\`\\`\\`"
-                }
-                return reply_args
+        # write code to temporary file (ideally in temporary directory)
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            scriptfile = Path(tmpdirname) / "script.py"
+            with open(scriptfile, "w", encoding="utf-8") as f:
+                f.write("\n".join(script))
+            try:  # now it's getting serious: get docker involved
+                reply_args = None
+                container_stderr = dockerclient.containers.run(
+                    image="manimcommunity/manim:stable",
+                    volumes={tmpdirname: {"bind": "/manim/", "mode": "rw"}},
+                    command=f"timeout 120 manim /manim/script.py -qm --disable_caching --progress_bar False -o scriptoutput {cli_flags}",
+                    user=os.getuid(),
+                    stderr=True,
+                    stdout=False,
+                    remove=True,
+                )
+                if container_stderr:
+                    if len(container_stderr.decode("utf-8")) <= 1200:
+                        reply_args = {
+                            "content": "Something went wrong, here is "
+                            "what Manim reports:\n"
+                            f"```\n{container_stderr.decode('utf-8')}\n```"
+                        }
+                    else:
+                        reply_args = {
+                            "content": "Something went wrong, here is "
+                            "what Manim reports:\n",
+                            "file": discord.File(
+                                fp=io.BytesIO(container_stderr),
+                                filename="Error.log",
+                            ),
+                        }
 
-            script = re.search(
-                pattern=r"```(?:py)?(?:thon)?(.*)```",
-                string=body,
-                flags=re.DOTALL,
-            ).group(1)
-            script = script.strip()
-
-            # for convenience: allow construct-only:
-            if script.startswith("def construct(self):"):
-                script = ["class Manimation(Scene):"] + [
-                    "    " + line for line in script.split("\n")
-                ]
-            else:
-                script = script.split("\n")
-
-            script = ["from manim import *"] + script
-
-            # write code to temporary file (ideally in temporary directory)
-            with tempfile.TemporaryDirectory() as tmpdirname:
-                scriptfile = Path(tmpdirname) / "script.py"
-                with open(scriptfile, "w", encoding="utf-8") as f:
-                    f.write("\n".join(script))
-                try:  # now it's getting serious: get docker involved
-                    reply_args = None
-                    container_stderr = dockerclient.containers.run(
-                        image="manimcommunity/manim:stable",
-                        volumes={tmpdirname: {"bind": "/manim/", "mode": "rw"}},
-                        command=f"timeout 120 manim /manim/script.py -qm --disable_caching --progress_bar False -o scriptoutput {cli_flags}",
-                        user=os.getuid(),
-                        stderr=True,
-                        stdout=False,
-                        remove=True,
-                    )
-                    if container_stderr:
-                        if len(container_stderr.decode("utf-8")) <= 1200:
-                            reply_args = {
-                                "content": "Something went wrong, here is "
-                                "what Manim reports:\n"
-                                f"```\n{container_stderr.decode('utf-8')}\n```"
-                            }
-                        else:
-                            reply_args = {
-                                "content": "Something went wrong, here is "
-                                "what Manim reports:\n",
-                                "file": discord.File(
-                                    fp=io.BytesIO(container_stderr),
-                                    filename="Error.log",
-                                ),
-                            }
-
-                        return reply_args
-
-                except Exception as e:
-                    reply_args = {"content": f"Something went wrong: ```{e}```"}
-                    raise e
-                finally:
-                    if reply_args:
-                        return reply_args
-
-                try:
-                    [outfilepath] = Path(tmpdirname).rglob("scriptoutput.*")
-                except Exception as e:
-                    reply_args = {
-                        "content": "Something went wrong: no (unique) output file was produced. :cry:"
-                    }
-                    raise e
-                else:
-                    reply_args = {
-                        "content": "Here you go:",
-                        "file": discord.File(outfilepath),
-                    }
-                finally:
                     return reply_args
 
-        async with ctx.typing():
-            reply_args = construct_reply(arg)
-            await ctx.reply(**reply_args)
+            except Exception as e:
+                reply_args = {"content": f"Something went wrong: ```{e}```"}
+                raise e
+            finally:
+                if reply_args:
+                    return reply_args
+
+            try:
+                [outfilepath] = Path(tmpdirname).rglob("scriptoutput.*")
+            except Exception as e:
+                reply_args = {
+                    "content": "Something went wrong: no (unique) output file was produced. :cry:"
+                }
+                raise e
+            else:
+                reply_args = {
+                    "content": "Here you go:",
+                    "file": discord.File(outfilepath),
+                }
+            finally:
+                return reply_args
