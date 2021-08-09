e75560d3a12b69d7f5a8bdb80033bb51db3d7fb8
ow0x <24418520+ow0x@users.noreply.github.com>
2021-08-09 17:40:37 +0530

    [badgetools] improvements
    * remove emojis code logic and instead borrow it from userinfo cog if its loaded
    
    * which make requiring badge emojis optional
    
    * remove meaningless status emojis too
    
    * don't allow normal users to snoop other guilds
    
    * fix error when bot is missing `add_reactions` perm in channel/guild
    
    * style + black + isort
    
    * reduce code debt

--- badgetools/badgetools.py
+++ badgetools/badgetools.py
@@ -1,187 +1,123 @@
-import datetime
-
 from collections import Counter
-from dateutil import relativedelta
+from datetime import datetime, timezone
 
-# Required by Red
 import discord
+from dateutil import relativedelta
 from redbot.core import commands
-from redbot.core.commands import GuildConverter
+from redbot.core.commands import Context, GuildConverter
 from redbot.core.utils import AsyncIter
-from redbot.core.utils.chat_formatting import inline, pagify
-from redbot.core.utils.menus import menu, DEFAULT_CONTROLS
+from redbot.core.utils.chat_formatting import pagify
+from redbot.core.utils.menus import close_menu, menu, DEFAULT_CONTROLS
 
 
 class BadgeTools(commands.Cog):
     """Various commands to show the stats about users' profile badges."""
 
-    __author__ = ["siu3334", "Fixator10"]
-    __version__ = "0.0.6"
+    __author__ = ["ow0x", "Fixator10"]
+    __version__ = "0.1.0"
 
-    def format_help_for_context(self, ctx: commands.Context) -> str:
+    def format_help_for_context(self, ctx: Context) -> str:
         """Thanks Sinbad!"""
         pre_processed = super().format_help_for_context(ctx)
         return f"{pre_processed}\n\nCog Version: {self.__version__}"
 
     def __init__(self, bot):
         self.bot = bot
-        self.emojis = self.bot.loop.create_task(self.init())
-        self.valid = [
-            "staff",
-            "partner",
-            "hypesquad",
-            "bug_hunter",
-            "hypesquad_bravery",
-            "hypesquad_brilliance",
-            "hypesquad_balance",
-            "early_supporter",
-            "team_user",
-            "system",
-            "bug_hunter_level_2",
-            "verified_bot",
-            "verified_bot_developer",
-        ]
 
-    def cog_unload(self):
-        if self.emojis:
-            self.emojis.cancel()
-
-    # Credits to flaree (taken from https://github.com/flaree/Flare-Cogs/blob/master/userinfo/userinfo.py#L33)
-    # Thanks flare <3
-    async def init(self):
-        await self.bot.wait_until_ready()
-        self.status_emojis = {
-            "mobile": discord.utils.get(self.bot.emojis, id=749067110931759185),
-            "online": discord.utils.get(self.bot.emojis, id=749221433552404581),
-            "away": discord.utils.get(self.bot.emojis, id=749221433095356417),
-            "dnd": discord.utils.get(self.bot.emojis, id=749221432772395140),
-            "offline": discord.utils.get(self.bot.emojis, id=749221433049088082),
-            "streaming": discord.utils.get(self.bot.emojis, id=749221434039205909),
-        }
-        self.badge_emojis = {
-            "staff": discord.utils.get(self.bot.emojis, id=848556248832016384),
-            "early_supporter": discord.utils.get(self.bot.emojis, id=706198530837970998),
-            "hypesquad_balance": discord.utils.get(self.bot.emojis, id=706198531538550886),
-            "hypesquad_bravery": discord.utils.get(self.bot.emojis, id=706198532998299779),
-            "hypesquad_brilliance": discord.utils.get(self.bot.emojis, id=706198535846101092),
-            "hypesquad": discord.utils.get(self.bot.emojis, id=706198537049866261),
-            "verified_bot_developer": discord.utils.get(self.bot.emojis, id=706198727953612901),
-            "bug_hunter": discord.utils.get(self.bot.emojis, id=848556247632052225),
-            "bug_hunter_level_2": discord.utils.get(self.bot.emojis, id=706199712402898985),
-            "partner": discord.utils.get(self.bot.emojis, id=848556249192202247),
-            "verified_bot": discord.utils.get(self.bot.emojis, id=848557763328344064),
-            "discord_certified_moderator": discord.utils.get(self.bot.emojis, id=848556248357273620),
-        }
+    def badge_emoji(self, badge_name: str):
+        cog = self.bot.get_cog("Userinfo")
+        if cog is None:
+            return f"{badge_name.replace('_', ' ').title()} :"
+        emoji = str(cog.badge_emojis[badge_name])
+        if "848561838974697532" in str(emoji):
+            emoji = "<:verified_bot:848557763328344064>"
+        return emoji
 
     @commands.command()
     @commands.guild_only()
     @commands.bot_has_permissions(embed_links=True)
-    async def badgecount(self, ctx: commands.Context, guild: GuildConverter = None):
-        """Shows the summary of badgecount of the server."""
+    async def badgecount(self, ctx: Context, guild: GuildConverter = None):
+        """Shows the count of user profile badges of the server."""
         guild = guild or ctx.guild
+        if not await self.bot.is_owner(ctx.author):
+            guild = ctx.guild
 
-        count = Counter()
-        # Credits to Fixator10 for improving this snippet
-        # Thanks Fixator <3
-        async for user in AsyncIter(guild.members):
-            async for flag in AsyncIter(user.public_flags.all()):
-                count[flag.name] += 1
+        async with ctx.typing():
+            count = Counter()
+            # Credits to Fixator10 for improving this snippet
+            # Thanks Fixator <3
+            async for user in AsyncIter(guild.members):
+                async for flag in AsyncIter(user.public_flags.all()):
+                    count[flag.name] += 1
 
-        msg = ""
-        pad = len(str(len(guild.members))) - 1
-        for k, v in sorted(count.items()):
-            msg += f"{self.badge_emojis[k]} `{str(v).zfill(pad)}` : {k.replace('_', ' ').title()}\n"
+            message = ""
+            fill = len(str(len(guild.members))) - 1
+            for k, v in sorted(count.items(), key=lambda x: x[1], reverse=True):
+                message += f"**{self.badge_emoji(k)}\u2000`{str(v).zfill(fill)}`**\n"
 
-        e = discord.Embed(colour=await ctx.embed_color())
-        e.set_footer(text=f"For Guild: {guild.name}", icon_url=str(guild.icon_url))
-        e.add_field(name="Badge Count:", value=msg)
-        await ctx.send(embed=e)
+            embed = discord.Embed(colour=await ctx.embed_color())
+            embed.set_author(name=str(guild.name), icon_url=str(guild.icon_url))
+            embed.description = message
+
+        await ctx.send(embed=embed)
 
     @commands.command()
     @commands.is_owner()
     @commands.guild_only()
-    @commands.bot_has_permissions(embed_links=True)
-    async def inbadge(self, ctx: commands.Context, badge: str, guild: GuildConverter = None):
+    @commands.bot_has_permissions(add_reactions=True, embed_links=True)
+    async def hasbadge(self, ctx: Context, *, badge: str):
         """Returns the list of users with X profile badge in the server."""
+        badge = badge.replace(" ", "_").lower()
 
-        guild = guild or ctx.guild
-
-        badgeslist = ", ".join(inline(m) for m in self.valid)
-        warn = f"Provided `badge` name is not valid! It needs to be either of:\n\n{badgeslist}"
-
-        if badge.lower() not in self.valid:
-            return await ctx.send(warn)
+        valid_flags = list(discord.PublicUserFlags.VALID_FLAGS.keys())
+        inform = "`{}` badge not found! It needs to be one of:\n\n{}".format(
+            badge, "\n".join([f"> `{x}`" for x in valid_flags])
+        )
+        if badge not in valid_flags:
+            return await ctx.send(inform)
 
         list_of = []
         # Thanks Fixator <3
-        async for user in AsyncIter(sorted(guild.members, key=lambda x: x.joined_at)):
+        async for user in AsyncIter(sorted(ctx.guild.members, key=lambda x: x.joined_at)):
             async for flag in AsyncIter(user.public_flags.all()):
                 if flag.name == badge:
-                    list_of.append(
-                        "{status}  {user_name_tag}\n".format(
-                            status=f"{self.status_emojis['mobile']}"
-                            if user.is_on_mobile()
-                            else f"{self.status_emojis['streaming']}"
-                            if any(
-                                a.type is discord.ActivityType.streaming
-                                for a in user.activities
-                            )
-                            else f"{self.status_emojis['online']}"
-                            if user.status.name == "online"
-                            else f"{self.status_emojis['offline']}"
-                            if user.status.name == "offline"
-                            else f"{self.status_emojis['dnd']}"
-                            if user.status.name == "dnd"
-                            else f"{self.status_emojis['away']}",
-                            user_name_tag=str(user),
-                        )
-                    )
-        output = "".join(m for m in list_of)
-        total = len([m for m in list_of])
+                    list_of.append(f"`{str(user):>20}` | {user.mention}")
+
+        output = "\n".join(list_of)
 
         embed_list = []
         for page in pagify(output, ["\n"], page_length=1000):
             em = discord.Embed(colour=await ctx.embed_color())
-            em.set_author(name=str(guild.name), icon_url=str(guild.icon_url))
+            em.set_author(name=str(ctx.guild.name), icon_url=str(ctx.guild.icon_url))
             em.description = page
-            em.set_footer(text=f"Found {total} users with {badge.replace('_', ' ').title()} badge!")
+            footer = f"Found {len(list_of)} users with {badge.replace('_', ' ').title()} badge!"
+            em.set_footer(text=footer)
             embed_list.append(em)
+
         if not embed_list:
-            return await ctx.send("I couldn't find any users with `{badge}` badge.")
-        elif len(embed_list) == 1:
-            return await ctx.send(embed=embed_list[0])
-        else:
-            await menu(ctx, embed_list, DEFAULT_CONTROLS, timeout=60.0)
+            return await ctx.send(f"I could not find any users with `{badge}` badge.")
+
+        controls = {"❌": close_menu} if len(embed_list) == 1 else DEFAULT_CONTROLS
+        await menu(ctx, embed_list, controls=controls, timeout=60.0)
 
     @commands.command()
     @commands.guild_only()
-    @commands.bot_has_permissions(embed_links=True)
-    async def boosters(self, ctx: commands.Context, guild: GuildConverter = None):
+    @commands.bot_has_permissions(add_reactions=True, embed_links=True)
+    async def boosters(self, ctx: Context, guild: GuildConverter = None):
         """Returns the list of active boosters of the server."""
-
         guild = guild or ctx.guild
-        if guild.premium_subscription_count == 0:
-            return await ctx.send(f"`{guild.name}` server doesn't have any boost(er)s yet.")
+        if not await self.bot.is_owner(ctx.author):
+            guild = ctx.guild
 
-        b_list = "{status}  `[since ~{since:>9}]`  {user_name_tag}"
+        if not guild.premium_subscribers:
+            return await ctx.send(f"`{guild.name}` does not have any boost(er)s yet.")
+
+        b_list = "`[since ~{since:>9}]`  `{user_name_tag:>20}` | {mention}"
         all_boosters = [
             b_list.format(
-                status=f"{self.status_emojis['mobile']}"
-                if user.is_on_mobile()
-                else f"{self.status_emojis['streaming']}"
-                if any(
-                    a.type is discord.ActivityType.streaming
-                    for a in user.activities
-                )
-                else f"{self.status_emojis['online']}"
-                if user.status.name == "online"
-                else f"{self.status_emojis['offline']}"
-                if user.status.name == "offline"
-                else f"{self.status_emojis['dnd']}"
-                if user.status.name == "dnd"
-                else f"{self.status_emojis['away']}",
-                user_name_tag=str(user),
                 since=self._relative_timedelta(user.premium_since),
+                user_name_tag=str(user),
+                mention=user.mention,
             )
             for user in sorted(guild.premium_subscribers, key=lambda x: x.premium_since)
         ]
@@ -198,14 +134,12 @@ class BadgeTools(commands.Cog):
             em.set_footer(text=footer)
             embed_list.append(em)
 
-        if len(embed_list) == 1:
-            return await ctx.send(embed=embed_list[0])
-        else:
-            await menu(ctx, embed_list, DEFAULT_CONTROLS, timeout=60.0)
+        controls = {"❌": close_menu} if len(embed_list) == 1 else DEFAULT_CONTROLS
+        await menu(ctx, embed_list, controls=controls, timeout=60.0)
 
     @staticmethod
     def _relative_timedelta(date_time):
-        dt1 = datetime.datetime.now(datetime.timezone.utc).replace(tzinfo=None)
+        dt1 = datetime.now(timezone.utc).replace(tzinfo=None)
         dt2 = date_time
 
         diff = relativedelta.relativedelta(dt1, dt2)
@@ -214,6 +148,6 @@ class BadgeTools(commands.Cog):
         hrs, mins, secs = (diff.hours, diff.minutes, diff.seconds)
 
         pretty = f"{yrs}y {mths}mth {days}d {hrs}h {mins}m {secs}s"
-        to_join = " ".join([x for x in pretty.split() if x[0] != '0'][:2])
+        to_join = " ".join([x for x in pretty.split() if x[0] != "0"][:2])
 
         return to_join
