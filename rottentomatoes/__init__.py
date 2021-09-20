42ef672bd5e2f8619ab4c55d7076a8afffab26fd
ow0x <24418520+ow0x@users.noreply.github.com>
2021-09-20 18:04:09 +0530

    remove rottentomatoes cog

--- rottentomatoes/__init__.py
+++
@@ -1,8 +0,0 @@
-from .rtomato import RottenTomatoes
-
-__red_end_user_data_statement__ = "This cog does not persistently store data about users."
-
-
-async def setup(bot):
-    n = RottenTomatoes(bot)
-    bot.add_cog(n)
