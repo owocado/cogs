93faea5a5c1efafeee79acac053bc9cb9bffbb93
ow0x <24418520+ow0x@users.noreply.github.com>
2021-10-17 12:06:33 +0530

    remove this cog
    I do simp for Cate Blanchett btw, wonderful person!
    https://youtu.be/JqCkGrgkzhQ?t=142

--- imagecog/__init__.py
+++
@@ -1,8 +0,0 @@
-from .imagecog import ImageCog
-
-__red_end_user_data_statement__ = "This cog does not persistently store data about users."
-
-
-def setup(bot):
-    n = ImageCog(bot)
-    bot.add_cog(n)
