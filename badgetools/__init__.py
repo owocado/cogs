70d95b1b32de714fae82b0b05a97a6ae2571964a
ow0x <24418520+ow0x@users.noreply.github.com>
2021-11-07 02:14:21 +0530

    Create __init__.py

---
+++ badgetools/__init__.py
@@ -0,0 +1,6 @@
+from .badgetools import BadgeTools
+
+__red_end_user_data_statement__ = "This cog does not persistently store any PII data about users."
+
+def setup(bot):
+    bot.add_cog(BadgeTools())
