5e3fc6464c6299916bef730ef5bb9315374b1c34
ow0x <24418520+ow0x@users.noreply.github.com>
2021-11-07 11:41:22 +0530

    Delete badgetools directory

--- badgetools/__init__.py
+++
@@ -1,8 +0,0 @@
-from .badgetools import BadgeTools
-
-__red_end_user_data_statement__ = "This cog does not persistently store data or metadata about users."
-
-
-async def setup(bot):
-    n = BadgeTools(bot)
-    bot.add_cog(n)
