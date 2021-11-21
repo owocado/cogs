8348340c36c209e8e1aa98f3c09f4bdfa9cb6f9a
ow0x <24418520+ow0x@users.noreply.github.com>
2021-11-22 03:26:49 +0530

    [moviedb] new amsterdam
    * new amsterdam
    
    * setup is not async here
    
    * dumb author
    
    also 10 bucks if someone pins the cog /j

--- moviedb/__init__.py
+++ moviedb/__init__.py
@@ -3,6 +3,5 @@ from .moviedb import MovieDB
 __red_end_user_data_statement__ = "This cog does not persistently store data about users."
 
 
-async def setup(bot):
-    n = MovieDB(bot)
-    bot.add_cog(n)
+def setup(bot):
+    bot.add_cog(MovieDB(bot))
