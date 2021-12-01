5a892ae6b571f79be8d719444dd05b14866a34d3
ow0x <24418520+ow0x@users.noreply.github.com>
2021-12-01 16:32:14 +0530

    move this crap to junk branch

--- stackoverflow/__init__.py
+++
@@ -1,8 +0,0 @@
-from .stackoverflow import StackOverflow
-
-__red_end_user_data_statement__ = "This cog does not persistently store data about users."
-
-
-def setup(bot):
-    n = StackOverflow(bot)
-    bot.add_cog(n)
