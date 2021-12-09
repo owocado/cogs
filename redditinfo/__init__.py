a8b5a1ee716fa429792b301274c9516044faccf9
ow0x <24418520+ow0x@users.noreply.github.com>
2021-12-09 12:56:22 +0530

    add bot in init

--- redditinfo/__init__.py
+++ redditinfo/__init__.py
@@ -1,7 +1,7 @@
 from .redditinfo import RedditInfo
 
-__red_end_user_data_statement__ = "This cog does not persistently store data about users."
+__red_end_user_data_statement__ = "This cog does not persistently store any PII data or metadata about users."
 
 
 def setup(bot):
-    bot.add_cog(RedditInfo())
+    bot.add_cog(RedditInfo(bot))
