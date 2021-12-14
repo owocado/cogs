9ea6ed4e89c03946fdfb614ad130a8167fca3e2d
ow0x <24418520+ow0x@users.noreply.github.com>
2021-12-14 14:31:35 +0530

    [kickstarter] refactor 2.0

--- kickstarter/kickstarter.py
+++ kickstarter/kickstarter.py
@@ -4,7 +4,7 @@ from datetime import datetime, timezone
 import aiohttp
 import discord
 from redbot.core import commands
-from redbot.core.utils.chat_formatting import humanize_number
+from redbot.core.utils.chat_formatting import humanize_number as hnum
 from redbot.core.utils.menus import close_menu, menu, DEFAULT_CONTROLS
 
 
@@ -18,63 +18,71 @@ class Kickstarter(commands.Cog):
         """Thanks Sinbad!"""
         pre_processed = super().format_help_for_context(ctx)
         return f"{pre_processed}\n\nAuthors: {self.__authors__}\nCog Version: {self.__version__}"
-    
+
     async def red_delete_data_for_user(self, **kwargs) -> None:
         """Nothing to delete"""
         pass
 
+    @staticmethod
+    async def get(ctx, base_url: str):
+        try:
+            async with aiohttp.request("GET", base_url) as response:
+                if response.status != 200:
+                    await ctx.send(f"https://http.cat/{response.status}")
+                    return None
+                return await response.json()
+        except asyncio.TimeoutError:
+            await ctx.send("Operation timed out.")
+            return None
+
+    @staticmethod
+    def make_embed(data, footer: str):
+        embed = discord.Embed(colour=0x14E06E, title=data.get("name", ""))
+        if data.get("urls", {}).get("web"):
+            embed.url = data.get("urls").get("web").get("project")
+        embed.set_author(name="Kickstarter", icon_url="https://i.imgur.com/EHDlH5t.png")
+        project_summary = data.get("blurb", "No summary.")
+        if data.get("photo"):
+            embed.set_image(url=data["photo"].get("full", ""))
+        embed.add_field(
+            name="Goal", value=f"{data.get('currency_symbol')}{hnum(round(data.get('goal', 0)))}",
+        )
+        pledged = f"{data.get('currency_symbol')}{hnum(round(data.get('pledged')))}"
+        percent_funded = round((data.get('pledged') / data.get('goal')) * 100)
+        pretty_pledged = f"{pledged}\n({hnum(percent_funded)}% funded)"
+        embed.add_field(name="Pledged", value=pretty_pledged)
+        embed.add_field(name="Backers", value=hnum(data.get("backers_count", 0)))
+        creator = f"[{data.get('creator').get('name')}]({data['creator']['urls']['web']['user']})"
+        deadline = datetime.utcfromtimestamp(data.get("deadline"))
+        past_or_future = "`**EXPIRED**`" if datetime.now(timezone.utc).replace(tzinfo=None) > deadline else ""
+        embed.description = (
+            f"{project_summary}\n\n**Creator**: {creator}\n"
+            f"**Creation Date**: <t:{int(data.get('created_at'))}:R>\n"
+            f"**Launched Date**: <t:{int(data.get('launched_at'))}:R>\n"
+            f"**Deadline**: <t:{int(data.get('deadline'))}:R> {past_or_future}\n"
+        )
+        if data.get("category"):
+            footer += f" | Category: {data.get('category').get('name')}"
+        embed.set_footer(text=footer)
+        return embed
+
     @commands.command()
     @commands.bot_has_permissions(embed_links=True)
-    @commands.cooldown(1, 3.0, commands.BucketType.member)
+    @commands.cooldown(1, 5.0, commands.BucketType.user)
     async def kickstarter(self, ctx: commands.Context, *, query: str):
         """Search for a project on Kickstarter."""
         base_url = f"https://www.kickstarter.com/projects/search.json?term={query}"
 
         await ctx.trigger_typing()
-        try:
-            async with aiohttp.request("GET", base_url) as response:
-                if response.status != 200:
-                    return await ctx.send(f"https://http.cat/{response.status}")
-                data = await response.json()
-        except asyncio.TimeoutError:
-            return await ctx.send("Operation timed out.")
-
+        data = await self.get(ctx, base_url)
+        if data is None: return
         if len(data["projects"]) == 0 and data["total_hits"] == 0:
             return await ctx.send(f"Could not find any results. Did you mean `{data['suggestion']}`?")
 
         pages = []
         for i, result in enumerate(data["projects"], start=1):
-            embed = discord.Embed(colour=0x14E06E)
-            embed.title = result.get("name")
-            if result.get("urls", {}).get("web"):
-                embed.url = result.get("urls").get("web").get("project")
-            embed.set_author(name="Kickstarter", icon_url="https://i.imgur.com/EHDlH5t.png")
-            project_summary = result.get("blurb", "No summary.")
-            if result.get("photo"):
-                embed.set_thumbnail(url=result.get("photo").get("full"))
-            embed.add_field(
-                name="Goal",
-                value=f"{result.get('currency_symbol')}{humanize_number(round(result.get('goal')))}",
-            )
-            pledged = f"{result.get('currency_symbol')}{humanize_number(round(result.get('pledged')))}"
-            percent_funded = round((result.get('pledged') / result.get('goal')) * 100)
-            pretty_pledged = f"{pledged}\n({humanize_number(percent_funded)}% funded)"
-            embed.add_field(name="Pledged", value=pretty_pledged)
-            embed.add_field(name="Backers", value=humanize_number(result.get("backers_count")))
-            creator = f"[{result.get('creator').get('name')}]({result['creator']['urls']['web']['user']})"
-            deadline = datetime.datetime.utcfromtimestamp(result.get("deadline"))
-            past_or_future = "`**EXPIRED**`" if datetime.now(timezone.utc).replace(tzinfo=None) > deadline else ""
-            embed.description = (
-                project_summary
-                + f"\n\n**Creator**: {creator}\n"
-                f"**Creation Date**: <t:{int(result.get('created_at'))}:R>\n"
-                f"**Launched Date**: <t:{int(result.get('launched_at'))}:R>\n"
-                f"**Deadline**: <t:{int(result.get('deadline'))}:R> {past_or_future}\n"
-            )
             footer = f"Page {i} of {len(data['projects'])}"
-            if result.get("category"):
-                footer += f" | Category: {result.get('category').get('name')}"
-            embed.set_footer(text=footer)
+            embed = self.make_embed(result, footer)
             pages.append(embed)
 
         controls = {"❌": close_menu} if len(pages) == 1 else DEFAULT_CONTROLS
