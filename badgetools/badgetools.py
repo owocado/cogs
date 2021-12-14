87e077e9bcb7f942283e942da30864592ac48ff4
ow0x <24418520+ow0x@users.noreply.github.com>
2021-12-14 19:51:10 +0530

    cringe cog ~ sourcery refactor
    localmemer more like localcringe*
    I hope MAX#1000 stop using my cogs like EVER1111 or I hope using my work/code only brings you severe bad luck
    dude keeps publicly trashtalking about my code yet at the same time uses it. yay for double standards :dogsmirk:
    but I guess trashtalking about anything and everything behind someone's back is your kind's birthright, very """advanced"""!

--- badgetools/badgetools.py
+++ badgetools/badgetools.py
@@ -8,14 +8,14 @@ from redbot.core.bot import Red
 from redbot.core.utils import AsyncIter
 from redbot.core.utils.chat_formatting import pagify
 from redbot.core.utils.menus import close_menu, menu, DEFAULT_CONTROLS
-# from redbot.core.utils.xmenus import BaseMenu, ListPages
+# from redbot.core.utils.dpy2_menus import BaseMenu, ListPages
 
 
 class BadgeTools(commands.Cog):
     """Various commands to show the stats about users' profile badges."""
 
     __author__ = "ow0x"
-    __version__ = "0.2.0"
+    __version__ = "0.3.0"
 
     def format_help_for_context(self, ctx: commands.Context) -> str:
         """Thanks Sinbad!"""
@@ -56,35 +56,31 @@ class BadgeTools(commands.Cog):
     @commands.bot_has_permissions(embed_links=True)
     async def badgecount(self, ctx: commands.Context):
         """Shows the count of user profile badges of the server."""
-        guild = ctx.guild
-
         async with ctx.typing():
             count = Counter()
             # Credits to Fixator10 for improving this snippet
             # Thanks Fixator <3
-            async for user in AsyncIter(guild.members):
+            async for user in AsyncIter(ctx.guild.members):
                 async for flag in AsyncIter(user.public_flags.all()):
                     count[flag.name] += 1
 
-            message = ""
-            fill = len(str(len(guild.members))) - 1
-            for k, v in sorted(count.items(), key=lambda x: x[1], reverse=True):
-                message += f"**{self.badge_emoji(k)}\u2000`{str(v).zfill(fill)}`**\n"
-
+            fill = len(str(len(ctx.guild.members))) - 1
+            message = "".join(
+                f"**{self.badge_emoji(k)}\u2000`{str(v).zfill(fill)}`**\n"
+                for k, v in sorted(count.items(), key=lambda x: x[1], reverse=True)
+            )
             embed = discord.Embed(colour=await ctx.embed_color())
-            embed.set_author(name=str(guild.name), icon_url=str(guild.icon_url))
+            embed.set_author(name=str(ctx.guild), icon_url=str(ctx.guild.icon_url))
             embed.description = message
 
         await ctx.send(embed=embed)
 
-    @commands.command(hidden=True)
     @commands.is_owner()
-    @commands.guild_only()
+    @commands.command(hidden=True)
     @commands.bot_has_permissions(add_reactions=True, embed_links=True)
     async def hasbadge(self, ctx: commands.Context, *, badge: str):
         """Returns the list of users with X profile badge in the server."""
         badge = badge.replace(" ", "_").lower()
-
         valid_flags = list(discord.PublicUserFlags.VALID_FLAGS.keys())
         inform = "`{}` badge not found! It needs to be one of:\n\n{}".format(
             badge, "\n".join([f"> `{x}`" for x in valid_flags])
@@ -100,22 +96,18 @@ class BadgeTools(commands.Cog):
                     list_of.append(f"{self.statusmoji(user)}{user}")
 
         output = "\n".join(list_of)
-
         pages = []
         for page in pagify(output, ["\n"], page_length=1000):
-            em = discord.Embed(colour=await ctx.embed_color())
-            em.set_author(name=str(ctx.guild.name), icon_url=str(ctx.guild.icon_url))
-            em.description = page
+            em = discord.Embed(colour=await ctx.embed_color(), description=page)
+            em.set_author(name=str(ctx.guild), icon_url=str(ctx.guild.icon_url))
             footer = f"Found {len(list_of)} users with {badge.replace('_', ' ').title()} badge!"
             em.set_footer(text=footer)
             pages.append(em)
 
         if not pages:
             return await ctx.send(f"I could not find any users with `{badge}` badge.")
-
         controls = {"❌": close_menu} if len(pages) == 1 else DEFAULT_CONTROLS
         await menu(ctx, pages, controls=controls, timeout=60.0)
-
         # For my bot which is on dpy2
         # await BaseMenu(ListPages(pages), ctx=ctx).start(ctx)
 
@@ -124,10 +116,8 @@ class BadgeTools(commands.Cog):
     @commands.bot_has_permissions(add_reactions=True, embed_links=True)
     async def boosters(self, ctx: commands.Context):
         """Returns the list of active boosters of the server."""
-        guild = ctx.guild
-
-        if not guild.premium_subscribers:
-            return await ctx.send(f"{guild.name} does not have any boost(er)s yet.")
+        if not ctx.guild.premium_subscribers:
+            return await ctx.send(f"{ctx.guild} does not have any boost(er)s yet.")
 
         b_list = "{status}`[since {since:>9}]`  {user_name_tag}"
         all_boosters = [
@@ -136,16 +126,15 @@ class BadgeTools(commands.Cog):
                 since=self._relative_timedelta(user.premium_since),
                 user_name_tag=str(user),
             )
-            for user in sorted(guild.premium_subscribers, key=lambda x: x.premium_since)
+            for user in sorted(ctx.guild.premium_subscribers, key=lambda x: x.premium_since)
         ]
         output = "\n".join(all_boosters)
-        footer = f"{guild.premium_subscription_count} boosts from {len(guild.premium_subscribers)} boosters!"
-
+        boosts, boosters = (ctx.guild.premium_subscription_count, len(ctx.guild.premium_subscribers))
         pages = []
         for page in pagify(output, ["\n"], page_length=1500):
-            em = discord.Embed(colour=await ctx.embed_color())
-            em.description = page
-            em.set_footer(text=footer)
+            em = discord.Embed(colour=await ctx.embed_color(), description=page)
+            em.set_author(name=str(ctx.guild), icon_url=str(ctx.guild.icon_url))
+            em.set_footer(text=f"{boosts} boosts  •  {boosters} boosters!")
             pages.append(em)
 
         controls = {"❌": close_menu} if len(pages) == 1 else DEFAULT_CONTROLS
@@ -156,13 +145,9 @@ class BadgeTools(commands.Cog):
     @staticmethod
     def _relative_timedelta(date_time):
         dt1 = datetime.now(timezone.utc).replace(tzinfo=None)
-
         diff = relativedelta.relativedelta(dt1, date_time)
 
         yrs, mths, days = (diff.years, diff.months, diff.days)
         hrs, mins, secs = (diff.hours, diff.minutes, diff.seconds)
-
         pretty = f"{yrs}y {mths}mo {days}d {hrs}h {mins}m {secs}s"
-        to_join = " ".join([x for x in pretty.split() if x[0] != "0"][:2])
-
-        return to_join
+        return " ".join([x for x in pretty.split() if x[0] != "0"][:2])
