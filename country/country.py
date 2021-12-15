93ea19d48b0dcee15e73edf04607d7e28a3510f4
ow0x <24418520+ow0x@users.noreply.github.com>
2021-12-15 15:14:59 +0530

    [country] sourcery refactor
    https://i.imgflip.com/5xy{9pg,cwh}.jpg

--- country/country.py
+++ country/country.py
@@ -2,13 +2,11 @@ import asyncio
 from urllib.parse import quote
 
 import aiohttp
-
-# Required by Red
 import discord
 from redbot.core import commands
 from redbot.core.utils.chat_formatting import humanize_number
-from redbot.core.utils.menus import menu, DEFAULT_CONTROLS
-# from redbot.core.utils.xmenus import BaseMenu, ListPages
+from redbot.core.utils.menus import DEFAULT_CONTROLS, close_menu, menu
+# from redbot.core.utils.dpy2_menus import BaseMenu, ListPages
 
 from .iso3166 import ALPHA3_CODES
 
@@ -16,13 +14,12 @@ from .iso3166 import ALPHA3_CODES
 class Country(commands.Cog):
     """Shows basic statistics and info about a country."""
 
-    __author__ = "ow0x"
-    __version__ = "0.0.8"
+    __author__ , __version__ = ("Author: ow0x", "Cog Version: 0.1.0")
 
     def format_help_for_context(self, ctx: commands.Context) -> str:
         """Thanks Sinbad!"""
         pre_processed = super().format_help_for_context(ctx)
-        return f"{pre_processed}\n\n Author: {self.__author__}\n Cog Version: {self.__version__}"
+        return f"{pre_processed}\n\n{self.__author__}\n{self.__version__}"
 
     @commands.command()
     @commands.bot_has_permissions(embed_links=True, add_reactions=True)
@@ -30,11 +27,10 @@ class Country(commands.Cog):
         """Fetch some basic statistics and info about a country."""
         await ctx.trigger_typing()
         try:
-            async with aiohttp.ClientSession() as session:
-                async with session.get(f"https://restcountries.com/v2/name/{name}") as response:
-                    if response.status != 200:
-                        return await ctx.send(f"https://http.cat/{response.status}")
-                    result = await response.json()
+            async with aiohttp.request("GET", f"https://restcountries.com/v2/name/{name}") as resp:
+                    if resp.status != 200:
+                        return await ctx.send(f"https://http.cat/{resp.status}")
+                    result = await resp.json()
         except asyncio.TimeoutError:
             return await ctx.send("Operation timed out.")
 
@@ -42,61 +38,54 @@ class Country(commands.Cog):
             return await ctx.send(f"{result['status']} {result['message']}")
 
         pages = []
-        for i, data in enumerate(result):
-            embed = discord.Embed(colour=await ctx.embed_color())
-            embed.title = data["name"]
-            embed.url = "https://en.wikipedia.org/wiki/" + quote(data["name"])
-            if data.get("flags"):
-                embed.set_thumbnail(url=data["flags"].get("png") or "")
-            embed.add_field(
-                name="Population", value=humanize_number(data.get("population", 0))
-            )
-            if data.get("area"):
-                embed.add_field(name="Area", value=f"{humanize_number(data['area'])} km²")
-            embed.add_field(name="Calling Code", value="+" + data.get("callingCodes")[0])
-            embed.add_field(name="Capital", value=f"\u200b{data.get('capital')}")
-            # Fix for Antarctica
-            if data.get("currencies"):
-                currencies = "\n".join([f"{x['name']} ({x['code']})" for x in data["currencies"]])
-                embed.add_field(name="Currency", value=currencies)
-            embed.add_field(
-                # "continent" key was changed to "region" now in API!
-                name="Region / Continent", value=f"{data['subregion']} / {data.get('region')}"
-            )
-            if data.get("topLevelDomain")[0] != "":
-                embed.add_field(name="Top Level Domain", value=data["topLevelDomain"][0])
-            if data.get("gini"):
-                embed.add_field(
-                    name="GINI Index",
-                    value=f"[{data.get('gini')}](https://en.wikipedia.org/wiki/Gini_coefficient)",
-                )
-            if data.get("demonym") != "":
-                embed.add_field(name="Demonym", value=data.get("demonym", "None"))
-            embed.add_field(name="Native Name", value=data.get("nativeName", "None"))
-            tzones = ", ".join(data.get("timezones"))
-            embed.add_field(name="Timezones", value=tzones)
-            if data.get("regionalBlocs"):
-                trade_blocs = ", ".join([x.get("name") for x in data["regionalBlocs"]])
-                embed.add_field(name="Part of Trade Bloc", value=trade_blocs)
-            if len(embed.fields) in [8, 11]:
-                embed.add_field(name="\u200b", value="\u200b")
-            description = ""
-            if data.get("borders"):
-                borders = "\n".join(
-                    [f"`[{i}]` {ALPHA3_CODES[x]}" for i, x in enumerate(data["borders"], 1)]
-                )
-                description += f"**Shares borders with:**\n{borders}\n\n"
-            if data.get("altSpellings"):
-                alt_names = ", ".join(data["altSpellings"])
-                description += f"**Other Names:** {alt_names}\n"
-            embed.description = description
-            embed.set_footer(
-                text=f"Page {i + 1} of {len(result)} • Powered by restcountries.com"
-            )
+        for i, data in enumerate(result, start=1):
+            colour = await ctx.embed_colour()
+            footer = f"Page {i} of {len(result)} • Powered by restcountries.com"
+            embed = self.country_embed(colour, footer, data)
             pages.append(embed)
 
-        if len(pages) == 1:
-            return await ctx.send(embed=pages[0])
-        else:
-            await menu(ctx, pages, DEFAULT_CONTROLS, timeout=60.0)
-            # await BaseMenu(ListPages(pages), ctx=ctx).start(ctx)
+        controls = {"": close_menu} if len(pages) == 1 else DEFAULT_CONTROLS
+        await menu(ctx, pages, controls=controls, timeout=90.0)
+        # await BaseMenu(ListPages(pages), ctx=ctx).start(ctx)
+
+    @staticmethod
+    def country_embed(colour, footer, data):
+        embed = discord.Embed(colour=colour, title=data.get("name", ""))
+        embed.url = "https://en.wikipedia.org/wiki/" + quote(data["name"])
+        if data.get("flags"):
+            embed.set_thumbnail(url=data["flags"].get("png", ""))
+        embed.add_field(name="Population", value=humanize_number(data.get("population", 0)))
+        if data.get("area"):
+            embed.add_field(name="Area", value=f"{humanize_number(data['area'])} km²")
+        embed.add_field(name="Calling Code", value="+" + data.get("callingCodes")[0])
+        embed.add_field(name="Capital", value=f"\u200b{data.get('capital')}")
+        # Fix for Antarctica
+        if data.get("currencies"):
+            currencies = "\n".join([f"{x['name']} ({x['code']})" for x in data["currencies"]])
+            embed.add_field(name="Currency", value=currencies)
+        # "continent" key was changed to "region" now in API!
+        embed.add_field(name="Region / Continent", value=f"{data['subregion']} / {data.get('region', '')}")
+        if data.get("topLevelDomain")[0] != "":
+            embed.add_field(name="Top Level Domain", value=data["topLevelDomain"][0])
+        wikilink = "https://en.wikipedia.org/wiki/Gini_coefficient"
+        if data.get("gini"):
+            embed.add_field(name="GINI Index", value=f"[{data.get('gini')}]({wikilink})")
+        if data.get("demonym"):
+            embed.add_field(name="Demonym", value=data.get("demonym", "None"))
+        embed.add_field(name="Native Name", value=data.get("nativeName", "None"))
+        tzones = ", ".join(data.get("timezones"))
+        embed.add_field(name="Timezones", value=tzones)
+        if data.get("regionalBlocs"):
+            trade_blocs = ", ".join([x.get("name") for x in data["regionalBlocs"]])
+            embed.add_field(name="Part of Trade Bloc", value=trade_blocs)
+        if len(embed.fields) in [8, 11]:
+            embed.add_field(name="\u200b", value="\u200b")
+        description = ""
+        if data.get("borders"):
+            borders = "\n".join([f"`[{i}]` {ALPHA3_CODES[x]}" for i, x in enumerate(data["borders"], 1)])
+            description += f"**Shares borders with:**\n{borders}\n\n"
+        if data.get("altSpellings"):
+            description += f"**Other Names:** {', '.join(data['altSpellings'])}\n"
+        embed.description = description
+        embed.set_footer(text=footer)
+        return embed
