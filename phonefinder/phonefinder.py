15d12932a45c0d6d8701421a6f5b68f10c503072
ow0x <24418520+ow0x@users.noreply.github.com>
2022-01-08 16:34:31 +0530

    [phonefinder] fix skill issue

--- phonefinder/phonefinder.py
+++ phonefinder/phonefinder.py
@@ -1,13 +1,13 @@
 import asyncio
+import urllib.parse
 from contextlib import suppress
-from urllib.parse import quote
+from typing import Optional, cast
 
 import aiohttp
 import discord
 from aiocache import SimpleMemoryCache, cached
 from bs4 import BeautifulSoup as bsp, element
 from redbot.core import commands
-from redbot.core.utils.menus import close_menu, menu
 
 BASE_URL = "https://www.gsmarena.com/results.php3?sQuickSearch=yes&sName={}"
 
@@ -21,7 +21,7 @@ HEAD = {
 class PhoneFinder(commands.Cog):
     """Fetch device specs for a (smart)phone model from GSMArena."""
 
-    __author__, __version__ = ("Author: ow0x", "Cog Version: 1.0.1")
+    __author__, __version__ = ("Author: ow0x", "Cog Version: 1.0.2")
 
     def format_help_for_context(self, ctx: commands.Context) -> str:
         """Thanks Sinbad!"""
@@ -36,7 +36,7 @@ class PhoneFinder(commands.Cog):
         asyncio.create_task(self.session.close())
 
     @cached(ttl=86400, cache=SimpleMemoryCache)
-    async def _fetch_href(self, ctx: commands.Context, query: str):
+    async def _fetch_href(self, ctx: commands.Context, query: str) -> Optional[str]:
         try:
             async with self.session.get(BASE_URL.format(query), headers=HEAD) as resp:
                 if resp.status != 200:
@@ -45,8 +45,9 @@ class PhoneFinder(commands.Cog):
         except asyncio.TimeoutError:
             return None
 
-        soup = bsp(data, "html.parser").find('div', {'class': "makers"})
-        makers = soup.find("ul").find_all("li")
+        soup = bsp(data, "html.parser").find("div", {"class": "makers"})
+        get_ul_div = cast(element.Tag, soup.find("ul"))
+        makers = get_ul_div.find_all("li")
         if not makers:
             return None
 
@@ -75,7 +76,7 @@ class PhoneFinder(commands.Cog):
         if choice is None or choice.content.strip() == "0":
             with suppress(discord.NotFound, discord.HTTPException):
                 await prompt.delete()
-            return 0
+            return "0"
         else:
             choice = choice.content.strip()
             choice = int(choice) - 1
@@ -88,8 +89,8 @@ class PhoneFinder(commands.Cog):
     @commands.bot_has_permissions(add_reactions=True, embed_links=True, read_message_history=True)
     async def phone(self, ctx: commands.Context, *, query: str):
         """Fetch device specs and other metadata about a (smart)phone model."""
-        endpoint = await self._fetch_href(ctx=ctx, query=quote(query))
-        if endpoint == 0:
+        endpoint = await self._fetch_href(ctx=ctx, query=urllib.parse.quote(query))
+        if endpoint == "0":
             return await ctx.send("OK! Operation cancelled.")
         if not endpoint:
             return await ctx.send("No results found for your query.")
@@ -112,14 +113,14 @@ class PhoneFinder(commands.Cog):
             return await ctx.send(html_title)
 
         def get_spec(query: str, key: str = "data-spec", cls: str = "td"):
-            result: element.Tag = soup.find(cls, {key: query})
+            result = soup.find(cls, {key: query})
             return result.text if result else "N/A"
 
         embed = discord.Embed(colour=await ctx.embed_colour())
         embed.title = str(get_spec("specs-phone-name-title", "class", "h1"))
         embed.url = url
-        embed.set_author(name="GSM Arena", icon_url="https://i.imgur.com/zi7jVhF.png")
-        phone_thumb: element.Tag = soup.find("div", {"class": "specs-photo-main"})
+        embed.set_author(name="GSM Arena", icon_url="https://i.imgur.com/lYfT1kn.png")
+        phone_thumb = cast(element.Tag, soup.find("div", {"class": "specs-photo-main"}))
         if phone_thumb:
             embed.set_thumbnail(url=str(phone_thumb.img.get("src")))
 
@@ -131,17 +132,17 @@ class PhoneFinder(commands.Cog):
         cpu_gpu = f"🧠  **CPU**: {get_spec('cpu')}\n" + f"╰⇢  **GPU**: {get_spec('gpu')}\n"
         battery = f"🔋  **Battery**: {get_spec('batdescription1')}\n\n"
         overview = release_date + body + phone_os + in_memory + storage_type + cpu_gpu + battery
+        mode = get_spec("cam1modules").replace("\n", " -> ")
         main_camera = (
-            "📸 **__MAIN CAMERA__**:\n"
-            + f"• **Mode**: {get_spec('cam1modules')}\n"
-            + f"• **Features**: {get_spec('cam1features')}\n"
-            + f"• **Video**: {get_spec('cam1video')}\n\n"
+            f"📸 **__MAIN CAMERA__**:\n• **Mode**: {mode}\n"
+            f"• **Features**: {get_spec('cam1features')}\n"
+            f"• **Video**: {get_spec('cam1video')}\n\n"
         )
         selfie_camera = (
             "📷 **__SELFIE CAMERA__**:\n"
-            + f"• **Mode**: {get_spec('cam2modules')}\n"
-            + f"• **Features**: {get_spec('cam2features')}\n"
-            + f"• **Video**: {get_spec('cam2video')}\n\n"
+            f"• **Mode**: {get_spec('cam2modules')}\n"
+            f"• **Features**: {get_spec('cam2features')}\n"
+            f"• **Video**: {get_spec('cam2video')}\n\n"
         )
         comms = (
             "📡 **__MISC. COMMS:__**\n"
@@ -159,8 +160,8 @@ class PhoneFinder(commands.Cog):
         embed.add_field(name="Resolution", value=str(get_spec("displayres-hl", cls="div")))
         embed.add_field(name="Chipset", value=str(get_spec("chipset-hl", cls="div")))
         fans = get_spec("help-fans", key="class", cls="li").split("\n")[2]
-        hits: element.Tag = soup.find("li", {"class": "help-popularity"})
+        hits = cast(element.Tag, soup.find("li", {"class": "help-popularity"}))
         embed.set_footer(
             text=f"Fans: {fans} • Popularity: 📈 +{hits.strong.text} ({hits.span.text})"
         )
-        await menu(ctx, [embed], controls={"❌": close_menu}, timeout=90.0)
+        await ctx.send(embed=embed)
