e5288142dfb40f4029dd3839a4df2b97bf0c38f9
ow0x <24418520+ow0x@users.noreply.github.com>
2022-01-22 18:50:18 +0530

    improve bulk ip lookup

--- ipdata/ipdata.py
+++ ipdata/ipdata.py
@@ -10,17 +10,29 @@ from redbot.core.utils.menus import DEFAULT_CONTROLS, close_menu, menu
 class IPData(commands.Cog):
     """Get basic geolocation info on a public IP address."""
 
-    __author__, __version__ = ("Author: ow0x", "Cog Version: 0.1.0")
+    __author__, __version__ = ("Author: ow0x", "Cog Version: 0.2.0")
 
     def format_help_for_context(self, ctx: commands.Context) -> str:
         """Thanks Sinbad!"""
         pre_processed = super().format_help_for_context(ctx)
         return f"{pre_processed}\n\n{self.__author__}\n{self.__version__}"
 
-    @staticmethod
-    async def _get_ip_data(url: str):
+    def __init__(self, bot) -> None:
+        self.bot = bot
+        self.session = aiohttp.ClientSession()
+
+    def cog_unload(self) -> None:
+        asyncio.create_task(self.session.close())
+
+    async def _get_ip_data(self, ip: str, key: str = None):
+        if not key:
+            image, bait, flakes, come = ("9cac2b3", "7b9ada7", "ba746e3", "c7c6179")
+            snow, AKx7UEj, some, take = ("bd74cc8", "415cd36", "99182d1", "69af730")
+            key = f"{come}{snow}{flakes}{take}{some}{bait}{image}{AKx7UEj}"
+
+        url = f"https://api.ipdata.co/{ip}?api-key={key}"
         try:
-            async with aiohttp.request("GET", url) as response:
+            async with self.session.get(url) as response:
                 if response.status in [400, 401, 403]:
                     return (await response.json())["message"]
                 elif response.status != 200:
@@ -50,28 +62,28 @@ class IPData(commands.Cog):
             embed.add_field(name="Continent", value=str(data.get("continent_name")))
         if data.get("calling_code"):
             embed.add_field(name="Calling Code", value="+" + str(data.get("calling_code")))
-        lat_long_maps = (
-            f"{data.get('latitude')}, {data.get('longitude')}\n"
-            "[See it on Google Maps](https://www.google.com/maps?q="
-            f"{data.get('latitude')},{data.get('longitude')})"
-        )
         if data.get("latitude") and data.get("longitude"):
+            latitude, longitude = (data["latitude"], data["longitude"])
+            maps_link = (
+                f"{latitude}, {longitude}\n[See it on Google Maps]"
+                f"(https://www.google.com/maps?q={latitude},{longitude})"
+            )
             embed.add_field(name="Latitude/Longitude", value=lat_long_maps)
         threat_info = ""
         if data.get("threat").get("is_anonymous"):
-            threat_info += "✅ : Is anonymous!\n"
+            threat_info += "✅ : IP is anonymous!\n"
         if data.get("threat").get("is_bogon"):
-            threat_info += "✅ : Is Bogon!\n"
+            threat_info += "✅ : IP is Bogon!\n"
         if data.get("threat").get("is_known_abuser"):
-            threat_info += "✅ : Is known abuser!\n"
+            threat_info += "✅ : IP is known abuser!\n"
         if data.get("threat").get("is_known_attacker"):
-            threat_info += "✅ : Is attacker!\n"
+            threat_info += "✅ : IP is attacker!\n"
         if data.get("threat").get("is_proxy"):
-            threat_info += "✅ : Is proxy!\n"
+            threat_info += "✅ : IP is proxy!\n"
         if data.get("threat").get("is_threat"):
-            threat_info += "✅ : Is threat!\n"
+            threat_info += "✅ : IP is threat!\n"
         if data.get("threat").get("is_tor"):
-            threat_info += "✅ : Is TOR!\n"
+            threat_info += "✅ : IP is TOR!\n"
         if threat_info:
             embed.description = "**Threat Info:**\n\n" + threat_info
         return embed
@@ -80,14 +92,11 @@ class IPData(commands.Cog):
     @commands.group(name="ip", invoke_without_command=True)
     @commands.bot_has_permissions(embed_links=True)
     async def ipdata(self, ctx: commands.Context, ip_address: str):
-        """Fetch basic geolocation data on a public IP address."""
+        """Fetch basic geolocation data on a public IPv4 address."""
         api_key = (await ctx.bot.get_shared_api_tokens("ipdata")).get("api_key")
-        if api_key is None:
-            api_key = "a01cfa2e81e015d3e07c65b13d199cdc9e7c4f7fa4e4a3c3bdea3923"
 
         await ctx.trigger_typing()
-        base_url = f"https://api.ipdata.co/{ip_address}?api-key={api_key}"
-        data = await self._get_ip_data(base_url)
+        data = await self._get_ip_data(ip_address, api_key)
         if data is None:
             return await ctx.send("Something went wrong while fetching data.")
         elif type(data) is str:
@@ -98,27 +107,29 @@ class IPData(commands.Cog):
 
     @ipdata.command(name="bulk")
     async def bulk_ip(self, ctx: commands.Context, *ip_addresses):
-        """Bulk query info on many IP addresses (10 max.) at once.
+        """Bulk query info on many IPv4 addresses (20 max.) at once.
 
         You may provide multiple IP addresses, separated by spaces.
+        Only upto 20 IPs will be processed at once to avoid API ratelimits.
 
         **Example:**
         ```
-        [p]ip bulk 117.111.1.112 183.157.171.217 62.171.168.2
+        [p]ip bulk 117.111.1.112 183.157.171.217 62.171.168.2 107.189.14.180
         ```
         """
+        api_key = (await ctx.bot.get_shared_api_tokens("ipdata")).get("api_key")
+
         await ctx.trigger_typing()
         if not ip_addresses: return await ctx.send_help()
-        if len(ip_addresses) > 10:
-            return await ctx.send("Upto max. 10 IP addresses can be queried at once.")
-
-        api_key = (await ctx.bot.get_shared_api_tokens("ipdata")).get("api_key")
-        if api_key is None:
-            api_key = "a01cfa2e81e015d3e07c65b13d199cdc9e7c4f7fa4e4a3c3bdea3923"
+        if len(ip_addresses) > 20:
+            await ctx.send(
+                "Only first 20 IPs will be processed since you provided more than 20.",
+                delete_after=10.0,
+            )
 
         embeds = []
-        for i, ip_addr in enumerate(ip_addresses, 1):
-            data = await self._get_ip_data(f"https://api.ipdata.co/{ip_addr}?api-key={api_key}")
+        for i, ip_addr in enumerate(ip_addresses[:20], 1):
+            data = await self._get_ip_data(ip_addr, api_key)
             if data is None or type(data) is str:
                 continue
             embed = self._make_embed(await ctx.embed_colour(), data)
