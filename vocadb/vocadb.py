820a813f3a0063494f9273e54e13c4f1c3b8a359
ow0x <24418520+ow0x@users.noreply.github.com>
2022-01-28 17:56:28 +0530

    various improvements

--- vocadb/vocadb.py
+++ vocadb/vocadb.py
@@ -1,103 +1,157 @@
-import aiohttp
 import asyncio
-import json
+import contextlib
 
 from datetime import datetime
-from typing import Any, Dict, Literal
 
+import aiohttp
 import discord
+from redbot.core import __version__ as redbot_version
 from redbot.core import commands
 from redbot.core.bot import Red
-from redbot.core.utils.chat_formatting import pagify
-from redbot.core.utils.menus import menu, DEFAULT_CONTROLS
+from redbot.core.utils.menus import DEFAULT_CONTROLS, close_menu, menu
+# from redbot.core.utils.dpy2_menus import BaseMenu, ListPages
 
 BASE_API_URL = "https://vocadb.net/api/songs"
-
-RequestType = Literal["discord_deleted_user", "owner", "user", "user_strict"]
 
 
 class VocaDB(commands.Cog):
     """Search for a song on Vocaloid Database (VocaDB) through a query"""
 
-    __author__ = "<@306810730055729152>"
-    __version__ = "0.0.2"
+    __author__, __version__ = ("Author: ow0x", "Cog Version: 0.1.0")
 
     def format_help_for_context(self, ctx: commands.Context) -> str:
         """Thanks Sinbad!"""
         pre_processed = super().format_help_for_context(ctx)
-        return f"{pre_processed}\n\nAuthor: {self.__author__}\nCog Version: {self.__version__}"
+        return f"{pre_processed}\n\n{self.__author__}\n{self.__version__}"
 
-    def __init__(self, bot: Red):
+    def __init__(self, bot: Red) -> None:
         self.bot = bot
+        self.session = aiohttp.ClientSession()
 
-    # credits to jack1142
-    async def red_get_data_for_user(self, *, user_id: int) -> Dict[str, Any]:
-        # this cog does not story any data
-        return {}
+    def cog_unload(self) -> None:
+        asyncio.create_task(self.session.close())
 
-    async def red_delete_data_for_user(
-        self, *, requester: RequestType, user_id: int
-    ) -> None:
-        # this cog does not story any data
+    async def red_delete_data_for_user(self, **kwargs) -> None:
+        """Nothing to delete"""
         pass
 
-    @commands.command()
-    async def vocadb(self, ctx, *, query: str):
-        """Search for a song on VocaDB"""
+    @staticmethod
+    def _parse_date(date_str: str) -> datetime:
+        """Parse a date string from VocaDB API"""
+        datetime_obj = datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%SZ")
+        return f"<t:{int(datetime_obj.timestamp())}:d>"
+
+    async def _fetch_data(self, ctx: commands.Context, query: str):
+        """Fetch data from VocaDB API and prompt the user to select an entry"""
         params = {
             "query": query,
-            "maxResults": 1,
+            "maxResults": 10,
             "sort": "FavoritedTimes",
             "preferAccurateMatches": "true",
             "nameMatchMode": "Words",
-            "fields": "ThumbUrl,Lyrics"
+            "fields": "Artists,Lyrics,Names,ThumbUrl"
         }
-        async with aiohttp.ClientSession() as session:
-            async with session.get(BASE_API_URL, params=params) as response:
-                if response.status == 200:
-                    result = await response.json(loads=json.loads)
-                else:
-                    return await ctx.send(f"API returned response code: {response.status}")
+        headers = {
+            "User-Agent": f"Red-DiscordBot/{redbot_version} owo-cogs/vocadb-{self.__version__}"
+        }
+        try:
+            async with self.session.get(BASE_API_URL, params=params, headers=headers) as resp:
+                if resp.status != 200:
+                    return f"https://http.cat/{resp.status}"
+                result = await resp.json()
+        except asyncio.TimeoutError:
+            return "Request timed out"
 
-        # Thanks to Fixator's suggestion
-        if data := result.get("items"):
-            data = data[0]
+        all_items = result.get("items", [])
+        if not all_items: return None
+        filtered_items = [x for x in all_items if x.get("lyrics")]
+        if not filtered_items: return None
+        if len(filtered_items) == 1: return filtered_items[0]
+
+        items = "\n".join(
+            f"**`[{i}]`** {x.get('defaultName')} - {x.get('artistString')}"
+            f" (published: {self._parse_date(x.get('publishDate'))})"
+            for i, x in enumerate(filtered_items, 1)
+        )
+        choices = f"Found below **{len(filtered_items)}** result(s). Pick one from:\n\n{items}"
+        prompt = await ctx.send(choices)
+
+        def check(msg: discord.Message) -> bool:
+            if (
+                msg.content.isdigit() and int(msg.content) in range(0, len(filtered_items) + 1)
+                and msg.author.id == ctx.author.id and msg.channel.id == ctx.channel.id
+            ):
+                return True
+            else:
+                return False
+
+        try:
+            choice = await self.bot.wait_for("message", timeout=60.0, check=check)
+        except asyncio.TimeoutError:
+            choice = None
+
+        if choice is None or choice.content.strip() == "0":
+            with contextlib.suppress(discord.NotFound, discord.HTTPException):
+                await prompt.edit("Cancelled.", delete_after=5.0)
+            return None
         else:
-            return await ctx.send("No results.")
-        if lyrics := data.get("lyrics"):
-            lyrics = lyrics[0].get("value")
-        else:
-            lyrics = "Lyrics unavailable."
-        mins = data.get("lengthSeconds", 0) // 60
-        secs = data.get("lengthSeconds", 0) % 60
-        pub_date = data.get("publishDate")
-        pub_date_strp = datetime.strptime(pub_date, "%Y-%m-%dT%H:%M:%SZ")
-        pub_date_strf = pub_date_strp.strftime("%A, %d %B, %Y")
+            choice = int(choice.content.strip()) - 1
+            with contextlib.suppress(discord.NotFound, discord.HTTPException):
+                await prompt.delete()
+            return filtered_items[choice]
+
+    @commands.command()
+    @commands.bot_has_permissions(embed_links=True)
+    async def vocadb(self, ctx: commands.Context, *, query: str):
+        """Search song lyrics from Vocaloid Database (VocaDB)"""
+        data = await self._fetch_data(ctx, query)
+        if type(data) == str:
+            return await ctx.send(data)
+        if not data:
+            return await ctx.send("No results found.")
+
+        await ctx.trigger_typing()
+        minutes = data.get("lengthSeconds", 0) // 60
+        seconds = data.get("lengthSeconds", 0) % 60
+        pub_date = self._parse_date(data.get("publishDate"))
+        all_artists = ", ".join(
+            f"[{x.get('name')}](https://vocadb.net/Ar/{x.get('id')}) ({x.get('categories')})"
+            for x in data.get("artists")
+        )
+        lang_map = {"en": "English", "ja": "Japanese", "": "Romaji"}
+        embed = discord.Embed(colour=await ctx.embed_colour())
+        embed.title = f"{data.get('defaultName')} - {data.get('artistString')}"
+        embed.url = f"https://vocadb.net/S/{data.get('id')}"
+        embed.set_thumbnail(url=data.get("thumbUrl", ""))
+        embed.add_field(name="Duration", value=f"{minutes} minutes, {seconds} seconds")
+        favorites, score = (data.get("favoritedTimes", 0), data.get("ratingScore", 0))
+        embed.add_field(name="Published On", value=pub_date)
+        embed.add_field(name="Statistics", value=f"{favorites} favourite(s), {score} total score")
+        embed.add_field(name="Artist(s)", value=all_artists)
+        embed.set_footer(text=f"Powered by VocaDB")
+        await ctx.send(embed=embed)
 
         embeds = []
-        for page in pagify(lyrics, ["\n"]):
+        for i, page in enumerate(data["lyrics"], 1):
+            lyrics_type = lang_map[page["cultureCode"]]
+            title = [
+                x.get("value") for x in data.get("names") if x.get("language") == lyrics_type
+            ]
             em = discord.Embed(
-                title=f"{data.get('defaultName')}",
+                title=title[0] if title else data.get("defaultName"),
                 colour=await ctx.embed_colour(),
             )
-            em.url = f"https://vocadb.net/S/{data.get('id')}"
-            em.description = page
-            em.set_thumbnail(url=str(data.get("thumbUrl")))
-            em.add_field(name="Duration", value=f"{mins} minutes, {secs} seconds")
-            em.add_field(name="Rating Score", value=str(data.get('ratingScore')))
-            em.add_field(
-                name="Favourited",
-                value=f"{str(data.get('favoritedTimes'))} times"
-                if data.get("favoritedTimes")
-                else "0 times"
+            em.set_thumbnail(url=data.get("thumbUrl") or "")
+            if data.get("id"):
+                em.url = f"https://vocadb.net/S/{data['id']}"
+            em.description = page.get("value", "")[:4090]
+            if page.get("source") and page.get("url"):
+                em.add_field(name="Source", value=f"[{page['source']}]({page['url']})")
+            em.set_footer(
+                text=f"Language: {lyrics_type} • Page {i} of {len(data['lyrics'])}"
             )
-            em.add_field(name="Artist", value=str(data.get("artistString")))
-            em.set_footer(text=f"Published date: {pub_date_strf}")
             embeds.append(em)
 
-        if not embeds:
-            return await ctx.send("No results.")
-        elif len(embeds) == 1:
-            return await ctx.send(embed=embeds[0])
-        else:
-            await menu(ctx, embeds, DEFAULT_CONTROLS, timeout=60.0)
+        controls = {"\u274c": close_menu} if len(embeds) == 1 else DEFAULT_CONTROLS
+        await menu(ctx, embeds, controls=controls, timeout=90.0)
+        # await BaseMenu(ListPages(embeds), timeout=90.0).start(ctx)
