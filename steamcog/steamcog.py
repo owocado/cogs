9d3c3234c69ca5ccbdd557a618e6925df5cce506
ow0x <24418520+ow0x@users.noreply.github.com>
2022-03-20 12:12:25 +0530

    [steamcog] style reformat

--- steamcog/steamcog.py
+++ steamcog/steamcog.py
@@ -1,29 +1,29 @@
 import asyncio
 import contextlib
 from datetime import datetime
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List, Optional, Union
 
 import aiohttp
 import discord
 from html2text import html2text
 from redbot.core import commands
 from redbot.core.bot import Red
-from redbot.core.utils.chat_formatting import bold, humanize_number
 from redbot.core.utils.menus import DEFAULT_CONTROLS, close_menu, menu
 # from redbot.core.utils.dpy2_menus import BaseMenu, ListPages
 
 from .stores import STORES
 
+PageType = List[Union[discord.Embed, str]]
 USER_AGENT = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"
-    " (KHTML, like Gecko) Chrome/98.0.4758.66 Safari/537.36"
+    " (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36"
 }
 
 
 class SteamCog(commands.Cog):
     """Get info about a Steam game and fetch cheap game deals for PC game(s)."""
 
-    __author__, __version__ = ("Author: ow0x", "Cog Version: 1.0.4")
+    __author__, __version__ = ("Author: ow0x", "Cog Version: 1.1.0")
 
     async def red_delete_data_for_user(self, **kwargs) -> None:
         """Nothing to delete"""
@@ -72,21 +72,27 @@ class SteamCog(commands.Cog):
     async def fetch_steam_game_id(self, ctx: commands.Context, query: str) -> Optional[int]:
         url = "https://store.steampowered.com/api/storesearch"
         data = await self.get(url, {"cc": "us", "l": "en", "term": query})
-        if not data: return None
-        if data.get("total", 0) == 0: return None
-        elif data.get("total") == 1: return data.get("items")[0].get("id")
+        if not data:
+            return None
+        if data.get("total", 0) == 0:
+            return None
+        elif data.get("total") == 1:
+            return data.get("items")[0].get("id")
         else:
             # Attribution: https://github.com/Sitryk/sitcogsv3/blob/master/lyrics/lyrics.py#L142
-            items = "\n".join(f"**{i}.** {val.get('name')}" for i, val in enumerate(data.get("items"), 1))
-            choices = f"Found below **{len(data['items'])}** results. Select one within 60 seconds:\n\n{items}"
+            items = "\n".join(
+                f"**{i}.** {val.get('name')}" for i, val in enumerate(data.get("items"), 1)
+            )
+            choices = f"Found below **{len(data['items'])}** results. Pick one in 60 seconds:\n\n{items}"
             prompt = await ctx.send(choices)
 
             def check(msg) -> bool:
                 return bool(
-                    msg.content.isdigit() and int(msg.content) in range(len(items) + 1)
-                    and msg.author.id == ctx.author.id and msg.channel.id == ctx.channel.id
+                    msg.content.isdigit()
+                    and int(msg.content) in range(len(items) + 1)
+                    and msg.author.id == ctx.author.id
+                    and msg.channel.id == ctx.channel.id
                 )
-
 
             try:
                 choice = await self.bot.wait_for("message", timeout=60.0, check=check)
@@ -112,7 +118,9 @@ class SteamCog(commands.Cog):
 
     def steam_embed(self, meta: tuple, app: Dict[str, Any]) -> discord.Embed:
         em = discord.Embed(
-            colour=meta[1], title=app["name"], description=app.get("short_description", ""),
+            colour=meta[1],
+            title=app["name"],
+            description=app.get("short_description", ""),
         )
         em.url = f"https://store.steampowered.com/app/{meta[0]}"
         em.set_author(name="Steam", icon_url="https://i.imgur.com/xxr2UBZ.png")
@@ -122,14 +130,17 @@ class SteamCog(commands.Cog):
         if app.get("release_date", {}).get("coming_soon"):
             em.add_field(name="Release Date", value="Coming Soon")
         else:
-            em.add_field(name="Release Date", value=self.timestamp(app["release_date"].get("date")))
+            em.add_field(
+                name="Release Date", value=self.timestamp(app["release_date"].get("date"))
+            )
         if app.get("metacritic"):
-            meta = app["metacritic"]
-            metacritic = f"**{meta.get('score')}** ([Critic Reviews]({meta.get('url')}))"
+            meta_ = app["metacritic"]
+            metacritic = f"**{meta_.get('score')}** ([Critic Reviews]({meta_.get('url')}))"
             em.add_field(name="Metacritic Score", value=metacritic)
         if app.get("recommendations"):
             em.add_field(
-                name="Recommendations", value=humanize_number(app["recommendations"].get("total")),
+                name="Recommendations",
+                value=f'{app["recommendations"].get("total"):,}',
             )
         if app.get("achievements"):
             em.add_field(name="Achievements", value=app["achievements"].get("total"))
@@ -167,13 +178,16 @@ class SteamCog(commands.Cog):
         else:
             app_id = await self.fetch_steam_game_id(ctx, query)
 
-        if app_id is None: return await ctx.send("Could not find any results.")
+        if app_id is None:
+            return await ctx.send("Could not find any results.")
         base_url = "https://store.steampowered.com/api/appdetails"
-        data = await self.get(base_url, {"appids": app_id, "l": "en", "cc": "us", "json": 1})
-        if not data: return await ctx.send("Something went wrong while querying Steam.")
+        data = await self.get(base_url, {"appids": str(app_id), "l": "en", "cc": "us", "json": 1})
+        if not data:
+            return await ctx.send("Something went wrong while querying Steam.")
         colour = await ctx.embed_colour()
         app_data = data[f"{app_id}"].get("data")
-        pages = [self.steam_embed((app_id, colour), app_data)]
+
+        pages: PageType = [self.steam_embed((app_id, colour), app_data)]
         if app_data.get("screenshots"):
             for i, preview in enumerate(app_data["screenshots"], start=1):
                 meta = (colour, app_id, app_data["name"])
@@ -186,15 +200,17 @@ class SteamCog(commands.Cog):
         # await BaseMenu(ListPages(pages), timeout=90, ctx=ctx).start(ctx)
 
     @staticmethod
-    def gamereqs_embed(meta, app: Dict[str, Any]) -> List[discord.Embed]:
-        pages = []
+    def gamereqs_embed(meta, app: Dict[str, Any]) -> PageType:
+        pages: PageType = []
         platform_mapping = {
             "windows": "pc_requirements",
             "mac": "mac_requirements",
             "linux": "linux_requirements",
         }
+
         def sanitise(rawline):
             return html2text(rawline).replace("\n\n", "\n")
+
         for key, value in app.get("platforms").items():
             if value and app.get(platform_mapping[key]):
                 em = discord.Embed(title=app["name"], colour=meta[1])
@@ -226,12 +242,13 @@ class SteamCog(commands.Cog):
             return await ctx.send("Could not find any results.")
 
         base_url = "https://store.steampowered.com/api/appdetails"
-        data = await self.get(base_url, {"appids": app_id, "l": "en", "cc": "us", "json": 1})
-        if not data: return await ctx.send("Something went wrong while querying Steam.")
+        data = await self.get(base_url, {"appids": str(app_id), "l": "en", "cc": "us", "json": 1})
+        if not data:
+            return await ctx.send("Something went wrong while querying Steam.")
 
         app_data = data[f"{app_id}"].get("data")
         meta = (app_id, await ctx.embed_colour())
-        pages = self.gamereqs_embed(meta, app_data)
+        pages: PageType = self.gamereqs_embed(meta, app_data)
         if not pages:
             return await ctx.send("Hmmm, no system requirements found for this game on Steam!")
 
@@ -242,23 +259,26 @@ class SteamCog(commands.Cog):
     async def fetch_deal_id(self, ctx: commands.Context, query: str) -> Optional[int]:
         url = f"https://www.cheapshark.com/api/1.0/games?title={query}"
         data = await self.get(url, None)
-        if not data: return None
-        if len(data) == 0: return None
-        elif len(data) == 1: return data[0].get("cheapestDealID")
+        if not data:
+            return None
+        if len(data) == 0:
+            return None
+        elif len(data) == 1:
+            return data[0].get("cheapestDealID")
         else:
             # Attribution: https://github.com/Sitryk/sitcogsv3/blob/master/lyrics/lyrics.py#L142
             items = "".join(f"**{i}.** {x.get('external')}\n" for i, x in enumerate(data[:20], 1))
             count = len(data) if len(data) <= 20 else 20
-            choices = f"Please select one from below:\n\n{items}"
-            send_to_channel = await ctx.send(f"Here are the first {count} results. {choices}")
+            choices = f"Please select one from below in 60 seconds:\n\n{items}"
+            prompt = await ctx.send(f"Here are the first {count} results. {choices}")
 
             def check(msg):
-                content = msg.content
-                if (
-                    msg.content.isdigit() and int(msg.content) in range(len(items) + 1)
-                    and msg.author.id == ctx.author.id and msg.channel.id == ctx.channel.id
-                ):
-                    return True
+                return bool(
+                    msg.content.isdigit()
+                    and int(msg.content) in range(len(items) + 1)
+                    and msg.author.id == ctx.author.id
+                    and msg.channel.id == ctx.channel.id
+                )
 
             try:
                 choice = await self.bot.wait_for("message", timeout=60.0, check=check)
@@ -266,10 +286,12 @@ class SteamCog(commands.Cog):
                 choice = None
 
             if choice is None or choice.content.strip() == "0":
-                await send_to_channel.delete()
+                with contextlib.suppress(discord.NotFound, discord.HTTPException):
+                    await prompt.edit(content="Cancelled.", delete_after=5.0)
                 return None
             else:
-                await send_to_channel.delete()
+                with contextlib.suppress(discord.NotFound, discord.HTTPException):
+                    await prompt.delete()
                 return data[int(choice.content.strip()) - 1].get("cheapestDealID")
 
     @staticmethod
@@ -286,18 +308,17 @@ class SteamCog(commands.Cog):
         final_deal = f"**{deal_price} USD**\n({discount}% discount)"
         em.add_field(name="Deal Price", value=final_deal)
         base = "https://cheapshark.com/redirect?dealID="
-        deal_store_info = f"[{bold(stores[game.get('storeID', '1')])}]({base}{deal_id})"
+        deal_store_info = f"[**{stores[game.get('storeID', '1')]}**]({base}{deal_id})"
         em.add_field(name="Deal available on", value=deal_store_info)
         if game.get("steamRatingPercent") != "0" and game.get("steamRatingText"):
             steam_rating = f"{game.get('steamRatingPercent')}%\n({game.get('steamRatingText')})"
             em.add_field(name="Rating", value=steam_rating)
         if data.get("cheapestPrice") and data["cheapestPrice"].get("price"):
             date_from_epoch = datetime.utcfromtimestamp(data["cheapestPrice"]["date"]).timestamp()
-            cheapest_price = (
-                f"{data['cheapestPrice'].get('price')} USD\nthat was:\n<t:{int(date_from_epoch)}:R>"
-            )
+            cheapest_price = f"{data['cheapestPrice'].get('price')} USD\nthat was:\n<t:{int(date_from_epoch)}:R>"
             em.add_field(name="Historical Cheapest Price", value=cheapest_price)
-        if len(em.fields) == 5: em.add_field(name="\u200b", value="\u200b")
+        if len(em.fields) == 5:
+            em.add_field(name="\u200b", value="\u200b")
         em.set_footer(text="Data provided by cheapshark.com")
         return em
 
@@ -306,12 +327,15 @@ class SteamCog(commands.Cog):
     async def gamedeal(self, ctx: commands.Context, *, game_name: str) -> None:
         """Fetch cheapest deal for a PC game from cheaphark.com"""
         deal_id = await self.fetch_deal_id(ctx, game_name)
-        if deal_id is None: return await ctx.send("No results.")
+        if deal_id is None:
+            return await ctx.send("No results.")
 
         await ctx.trigger_typing()
         deal_url = f"https://www.cheapshark.com/api/1.0/deals?id={deal_id}"
         data = await self.get(deal_url, None)
-        if not data: return await ctx.send("Something went wrong while querying CheapShark!")
+        if not data:
+            return await ctx.send("\u26d4 Something went wrong while querying CheapShark API!")
+
         all_stores = await self.get("https://www.cheapshark.com/api/1.0/stores", None)
         if all_stores:
             STORES = {x["storeID"]: x["storeName"] for x in all_stores}
@@ -337,14 +361,15 @@ class SteamCog(commands.Cog):
             final_deal = f"**{deal_price} USD**\n({discount}% discount)"
             em.add_field(name="Deal Price", value=final_deal)
             deal_store_info = (
-                f"[{bold(stores[data.get('storeID')])}]"
+                f"[**{stores[data.get('storeID')]}**]"
                 + f"(https://cheapshark.com/redirect?dealID={data['dealID']} 'Click here to go to this deal')"
             )
             em.add_field(name="Deal available on", value=deal_store_info)
         if data.get("steamRatingPercent") != "0" and data.get("steamRatingText"):
             steam_rating = f"{data.get('steamRatingPercent')}% ({data.get('steamRatingText')})"
             em.add_field(name="Rating", value=steam_rating)
-        if len(em.fields) == 5: em.add_field(name="\u200b", value="\u200b")
+        if len(em.fields) == 5:
+            em.add_field(name="\u200b", value="\u200b")
         em.set_footer(text=f"Page {meta[0]} of {meta[1]} | Data provided by cheapshark.com")
         return em
 
@@ -357,10 +382,19 @@ class SteamCog(commands.Cog):
         `sort_by` argument accepts only one of the following parameter:
         `deal rating`, `title`, `savings`, `price`, `metacritic`, `reviews`, `release`, `store`, `recent`
         """
-        if sort_by not in [
-            "title", "savings", "price", "metacritic", "reviews", "release", "store", "recent", "deal rating"
+        allowed_sorts = [
+            "title",
+            "savings",
+            "price",
+            "metacritic",
+            "reviews",
+            "release",
+            "store",
+            "recent",
+            "deal rating",
         ]:
-            return await ctx.send("Invalid sorting option given. See commands help!")
+        if sort_by not in allowed_sorts:
+            return await ctx.send(f"`sort_by` can only be one of `{', '.join(allowed_sorts)}`")
 
         base_url = f"https://www.cheapshark.com/api/1.0/deals?sortBy={sort_by}"
         await ctx.trigger_typing()
@@ -368,9 +402,11 @@ class SteamCog(commands.Cog):
         all_stores = await self.get("https://www.cheapshark.com/api/1.0/stores", None)
         if all_stores:
             STORES = {x["storeID"]: x["storeName"] for x in all_stores}
-        if not results: return await ctx.send("Something went wrong while querying CheapShark!")
 
-        pages = []
+        if not results:
+            return await ctx.send("\u26d4 Something went wrong while querying CheapShark API!")
+
+        pages: PageType = []
         for i, data in enumerate(results, 1):
             meta = (i, len(results), await ctx.embed_color())
             em = self.latestdeals_embed(meta, STORES, data)
