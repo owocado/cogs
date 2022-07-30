16449acddf30e5301a830f427cbedc35d931a139
ow0x <24418520+ow0x@users.noreply.github.com>
2022-07-30 19:45:30 +0530

    Update jsk_cog.py

--- jsk/jsk_cog.py
+++ jsk/jsk_cog.py
@@ -13,13 +13,12 @@ class Jishaku(*STANDARD_FEATURES, *OPTIONAL_FEATURES):
     """Jishaku, a debugging and testing cog for discord.py rewrite bots."""
 
     __authors__ = ["Gorialis", "ow0x"]
-    __version__ = "2.5.0"
+    __version__ = "2.5.1"
 
-    def format_help_for_context(self, ctx: commands.Context) -> str:
-        """Thanks Sinbad."""
+    def format_help_for_context(self, ctx: commands.Context) -> str:  # Thanks Sinbad!
         authors = map(lambda x: f'[{x}](https://github.com/{x})', self.__authors__)
         return (
             f"{super().format_help_for_context(ctx)}\n\n"
-            f"Authors:  {', '.join(authors)}\n"
-            f"Cog version:  v{self.__version__}"
+            f"**Authors:**  {', '.join(authors)}\n"
+            f"**Cog version:**  v{self.__version__}"
         )
