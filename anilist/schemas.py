d3c5b6c1bf46cc6c5dee739f8b99532c2b978f20
ow0x <24418520+ow0x@users.noreply.github.com>
2022-08-16 18:09:52 +0530

    to err is human, sigh

--- anilist/schemas.py
+++ anilist/schemas.py
@@ -1,38 +1,37 @@
 CHARACTER_SCHEMA = """
-query ($page: Int, $perPage: Int, $search: String) {
+query ($page: Int, $perPage: Int, $search: String, $sort: [CharacterSort]) {
   Page(page: $page, perPage: $perPage) {
-    users(search: $search) {
-      id
-      name
-      about(asHtml: false)
-      avatar {
+    characters(search: $search, sort: $sort) {
+      name {
+        full
+        native
+        alternative
+      }
+      image {
         large
       }
-      bannerImage
-      siteUrl
-      previousNames {
-        name
-        createdAt
-        updatedAt
+      description(asHtml: false)
+      gender
+      dateOfBirth {
+        year
+        month
+        day
       }
-      statistics {
-        anime {
-          count
-          meanScore
-          minutesWatched
-          episodesWatched
-        }
-        manga {
-          count
-          meanScore
-          chaptersRead
-          volumesRead
+      age
+      siteUrl
+      media(perPage: 10) {
+        nodes {
+          siteUrl
+          type
+          title {
+            english
+            romaji
+          }
         }
       }
     }
   }
 }
-
 """
 
 
